90bc9871dfaba9f0efc4a2e1f15d62d1847a5a61
Akash Kothari <akashk4@illinois.edu>
2021-09-02 15:50:59 -0500

    Add vector instructions to DSL

--- dsl.rkt
+++ dsl.rkt
@@ -1,3 +1,4 @@
+;; Vector mac
 (define (vector-mac v-acc v1 v2)
   (assert (= (length v1)
              (length v2)
@@ -8,8 +9,10 @@
              [e2 v2])
     (box (+ (unbox e-acc) (* (unbox e1) (unbox e2))))))
 
-
+;; Vector mac reduction
 (define (vector-mac-reduction v-acc v1 v2 len)
+    (assert (= (length v1) (length v2))
+          "vector-mac-reduction: length of vectors not equal")
   (for ([i (length v1)])
     (define sum
       (apply
@@ -17,3 +20,35 @@
        (for ([j len])
          (* (unbox v1[(+ j (* i len))]) (unbox v2[(+ j (* i len))])))))
     (set-box! (+ (unbox v-acc[i]) (sum)))))
+
+;; Vector add
+(define (vector-add v1 v2)
+  (assert (= (length v1) (length v2))
+          "vector-add: length of vectors not equal")
+  (for/list ([e1 v1]
+             [e2 v2])
+    (box (+ (unbox e1) (unbox e2)))))
+
+;; Vector sub
+(define (vector-sub v1 v2)
+  (assert (= (length v1) (length v2))
+          "vector-sub: length of vectors not equal")
+  (for/list ([e1 v1]
+             [e2 v2])
+    (box (- (unbox e1) (unbox e2)))))
+
+;; Vector div
+(define (vector-div v1 v2)
+  (assert (= (length v1) (length v2))
+          "vector-div: length of vectors not equal")
+  (for/list ([e1 v1]
+             [e2 v2])
+    (box (/ (unbox e1) (unbox e2)))))
+
+;; Vector mul
+(define (vector-mul v1 v2)
+  (assert (= (length v1) (length v2))
+          "vector-mul: length of vectors not equal")
+  (for/list ([e1 v1]
+             [e2 v2])
+    (box (* (unbox e1) (unbox e2)))))
