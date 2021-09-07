ec7364658caa4a08f77828ca7729adc4dd8e2afd
Akash Kothari <akashk4@illinois.edu>
2021-09-07 17:46:19 -0500

    Create utils.rkt

---
+++ utils.rkt
@@ -0,0 +1,15 @@
+#lang rosette
+
+;; Function to extract a bitvector
+;; depending on the given type size
+(define (ext-bv x i type-size)
+  (define var (extract (+ (* i type-size) (- type-size 1)) (* i type-size) x))
+  var)
+
+(define (sign-ext-bv x i type-size target-size)
+  (define var (sign-extend (ext-bv x i type-size) (bitvector target-size)))
+  var)
+
+(define (zero-ext-bv x i type-size target-size)
+  (define var (zero-extend (ext-bv x i type-size) (bitvector target-size)))
+  var)
