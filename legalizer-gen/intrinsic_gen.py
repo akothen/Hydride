8e3aaf26f80685ebfd2651f59e01a6cee7c7d36b
Akash Kothari <akashk4@illinois.edu>
2021-09-16 13:25:06 -0500

    fix a bug

--- legalizer-gen/intrinsic_gen.py
+++ legalizer-gen/intrinsic_gen.py
@@ -1,4 +1,7 @@
+
+
 from equivalence_checker import gen_equivalence_checker 
+
 
 spec_file = "spec.rkt" 
 sketch_file = "sketch.rkt"
@@ -17,18 +20,17 @@ def gen_intrinsic(id, num_vectors, num_sym_ints, num_concrete_vals):
   int_ty = "llvm_anyint_ty"
   output = "[" + vector_ty + "]"
   input_list = ""
+  num_ints = num_sym_ints + num_concrete_vals
   for i in range(num_vectors):
     input_list += vector_ty
     if i != num_vectors - 1:
       input_list += ","
-  for i in range(num_sym_ints):
+  if len(input_list) != 0 and num_ints != 0:
+    input_list += ","
+  for i in range(num_ints):
     input_list += int_ty
-    if i != num_vectors - 1:
+    if i != num_ints - 1:
       input_list += ","
-  for i in range(num_concrete_vals):
-    input_list += int_ty
-    if i != num_vectors - 1:
-      input_list += "],"
   input = "[" + input_list + "]"
   attr = "[IntrNoMem, IntrSpeculatable]"
   name = "intrinsic" + str(id)
