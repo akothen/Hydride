ed41249f3de7ef9bfd24fda0075a1e43ae14b4f7
Akash Kothari <akashk4@illinois.edu>
2021-09-23 12:26:30 -0500

    expand dictionary

--- dsl-synthesizer-gen/tensor_dsl.py
+++ dsl-synthesizer-gen/tensor_dsl.py
@@ -4,56 +4,68 @@ dsl = {
   "vector-mac" : {
     "name" : "vector-mac",
     "args" : "dst, a, b, len, precision",
+    "reg" : "dst, a, b",
+    "size" : "len",
+    "in_precision" : "precision",
+    "out_precision" : "precision",
     "semantics" : "{   \
-                (define (vector-mac dst a b len precision) \
-                  (begin  \
-                  (assert (bv? dst))  \
-                  (assert (bv? a))  \
-                  (assert (bv? b))  \
-                  (apply  \
-                  concat  \
-                  (for/list ([j (range len)])  \
-                    (define tmp  \
-                      (bvmul (ext-bv a (- (- len 1) j) precision) (ext-bv b (- (- len 1) j) precision))) \
-                    (bvadd (ext-bv dst (- (- len 1) j) precision) tmp) \
-                    ) \
-                  ) \
-                  ) \
-                ) \
-              }",
+                    (define (vector-mac dst a b len precision) \n \
+                      (begin  \n \
+                      (assert (bv? dst))  \n \
+                      (assert (bv? a))  \n \
+                      (assert (bv? b))  \n \
+                      (apply  \n \
+                      concat  \n \
+                      (for/list ([j (range len)])  \n \
+                        (define tmp  \n \
+                          (bvmul (ext-bv a (- (- len 1) j) precision) (ext-bv b (- (- len 1) j) precision))) \n \
+                        (bvadd (ext-bv dst (- (- len 1) j) precision) tmp) \n \
+                        ) \n \
+                      ) \n \
+                      ) \n \
+                    ) \n \
+                  }",
   },
 
   "vector-add" : {
     "name" : "vector-add",
     "args" : "a, b, len, precision",
+    "reg" : "a, b",
+    "size" : "len",
+    "in_precision" : "precision",
+    "out_precision" : "precision",
     "semantics" : "{   \
-                (define (vector-add a b len precision) \
-                  (apply \
-                  concat \
-                  (for/list ([j (range len)]) \
-                    (define tmp \
-                      (bvadd (ext-bv a (- (- len 1) j) precision) (ext-bv b (- (- len 1) j) precision))) \
-                    tmp \
-                    ) \
-                  ) \
-                  ) \
-              }",
+                    (define (vector-add a b len precision) \n \
+                      (apply \n \
+                      concat \n \
+                      (for/list ([j (range len)]) \n \
+                        (define tmp \n \
+                          (bvadd (ext-bv a (- (- len 1) j) precision) (ext-bv b (- (- len 1) j) precision))) \n \
+                        tmp \n \
+                        ) \n \
+                      ) \n \
+                      ) \n \
+                 }",
   },
 
   "vector-sub" : {
     "name" : "vector-sub",
     "args" : "a, b, len, precision",
+    "reg" : "a, b",
+    "size" : "len",
+    "in_precision" : "precision",
+    "out_precision" : "precision",
     "semantics" : "{   \
-                (define (vector-sub a b len precision) \
-                  (apply \
-                  concat \
-                  (for/list ([j (range len)]) \
-                    (define tmp \
-                      (bvsub (ext-bv a (- (- len 1) j) precision) (ext-bv b (- (- len 1) j) precision))) \
-                    tmp \
-                    ) \
-                  ) \
-                  ) \
-              }",
+                    (define (vector-sub a b len precision) \n \
+                      (apply \n \
+                      concat \n \
+                      (for/list ([j (range len)]) \n \
+                        (define tmp \n \
+                          (bvsub (ext-bv a (- (- len 1) j) precision) (ext-bv b (- (- len 1) j) precision))) \n \
+                        tmp \n \
+                        ) \n \
+                      ) \n \
+                      ) \n \
+                  }",
   },
 }
