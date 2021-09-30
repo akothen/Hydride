635e48b31c307617a87b041f4422cd29d593a1c9
Akash Kothari <akashk4@illinois.edu>
2021-09-30 12:32:54 -0500

    Update pattern_gen.py

--- legalizer-gen/pattern_gen.py
+++ legalizer-gen/pattern_gen.py
@@ -1,43 +1,51 @@
 
+from dslinfo import DSLInstInfo
+from utility import list_to_string
 
 
-def gen_pattern(tensor_intrinsic_name, num_args):
-  arg_string = ""
-  for i in range(num_args):
-    if arg_string != "":
-      arg_string += ","
-    arg_string += "m_Value(V_" + str(i) + ")"
-  string = "match(II, m_Intrinsic<Intrinsic::" + tensor_intrinsic_name + "(" + arg_string + "))"
+def gen_inst_map_entry(inst_spec_map, inst_sema_map, inst_name):
+  dsl_info = DSLInstInfo(inst_spec_map[inst_name])
+  name = "\"" + inst_name + "\""
+  args = dsl_info.get_args()
+  args_string = list_to_string(args)
+  regs_string = inst_sema_map[inst_name]["args"]
+  size_string = inst_sema_map[inst_name]["bitwidth"]
+  in_precision_string = inst_sema_map[inst_name]["input_precision"]
+  out_precision_string = inst_sema_map[inst_name]["output_precision"]
+  semantics = dsl_info.get_semantics()
+  semantics_string = "\"" + semantics + "\""
+  return f'''
+      "name" : {name},
+      "args" : {args_string},
+      "reg"  : {regs_string},
+      "bitwidth" : {size_string},
+      "in_precision" : {in_precision_string},
+      "out_precision" : {out_precision_string},
+      "semantics" : {semantics_string}, 
+  '''
+
+
+def gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, inst_list):
+  string = ""
+  for inst_name in inst_list:
+    entry = gen_inst_map_entry(inst_spec_map, inst_sema_map, inst_name)
+    name = "\"" + inst_name + "\""
+    key = name + " : {\n"
+    full_entry = key + entry + "},\n\n"
+    string += full_entry
+  string += "},"
   return string
 
 
-def gen_intrinsic(target_intrinsic_name, shape_m, shape_n):
-  code = "IRBuilder<> Builder(II);\n"
-  code += "auto &Ctx = II->getParent()->getContext();\n"
-  code += "auto *Int16Ty = Type::getInt16Ty(Ctx);\n"
-  code += "auto *ShapeM = ConstantInt::get(Int16Ty, " + str(shape_m) + ");\n"
-  code += "auto *ShapeN = ConstantInt::get(Int16Ty, " + str(shape_n) + ");\n"
-  code += "std::vector<Value *> Args;\n"
-  code += "Args.insert(Args.end(), {ShapeM, ShapeN});\n"
-  intrinsic_code = "Builder.CreateIntrinsic(Intrinsic::" + target_intrinsic_name + ", None, ArrayRef<Value *>(Args));\n"
-  code += "auto *LI = " +  intrinsic_code
-  code += "ToBeRemoved.insert(II); \n"
-  code += "InstToInstMap[II] = LI; \n"
-  code += "return true;"
-  return code
+def gen_intrinsic_target_inst_map(intrinsic_to_inst_map, inst_spec_map, inst_sema_map, legalizer_file):
+  string = "\n\nintrinsics = {\n"
+  for intrisic_name, inst_list in intrinsic_to_inst_map.items():
+    name = "\n\"" + intrisic_name +  "\""
+    key = name + " : {\n"
+    entry = gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, inst_list)
+    full_entry = key + entry + "\n},\n\n"
+    string += full_entry
+  string += "}"
+  f = open(legalizer_file, "w")
+  f.write(string)
 
-
-def gen_function_body(target_intrinsic_name, shape_m, shape_n, tensor_intrinsic_name, num_args):
-  code = "if (" + gen_pattern(tensor_intrinsic_name, num_args) + ") {\n"
-  code += gen_intrinsic(target_intrinsic_name, shape_m, shape_n)
-  code += "}"
-  return code
-
-
-def gen_function():
-  code = "bool legalize(IntrinsicInst *II) {\n"
-  code += gen_function_body("x86_tdpbssd_internal", 16, 16, "tensor_mma", 4)
-  code += "}"
-  print(code)
-
-#gen_function()
