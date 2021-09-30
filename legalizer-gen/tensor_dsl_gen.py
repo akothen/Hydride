790e491ffc19f1271be6734d68e2eb0aa35d7422
Akash Kothari <akashk4@illinois.edu>
2021-09-30 07:23:43 -0500

    Update tensor_dsl_gen.py

--- legalizer-gen/tensor_dsl_gen.py
+++ legalizer-gen/tensor_dsl_gen.py
@@ -1,4 +1,37 @@
 
+
+def find_between(s, first, last):
+  find_bw_array = list()
+  # Split the string 
+  s_array = s.split(first)
+  print("s_array:")
+  print(s_array)
+  print(len(s_array))
+  for string in s_array:
+    string = first + string
+    print("string:")
+    print(string)
+    if string not in s:
+      continue
+    start = string.index(first) + len(first)
+    print("start:")
+    print(start)
+    try:
+      end = string.index(last, start)
+    except ValueError:
+      continue
+    print("end:")
+    print(end)
+    ret = string[start:end]
+    print("ret:")
+    print(ret)
+    if ret == '':
+      continue
+    if ret not in find_bw_array:
+      find_bw_array.append(ret)
+  return find_bw_array
+
+
 ext_utility_funcs = [
   "ext-bv", "sign-ext-bv", "zero-ext-bv", 
   "low-ext-bv", "sign-low-ext-bv", "zero-low-ext-bv"
@@ -127,42 +160,10 @@ class DSLInstInfo:
     new_form = ""
     for line in inst_sema_lines:
       new_form += ((line + "\\n \\\n"))
+    new_form = "{ \\\n" + new_form + "}"
     return new_form
   
-
-
-def find_between(s, first, last):
-  find_bw_array = list()
-  # Split the string 
-  s_array = s.split(first)
-  print("s_array:")
-  print(s_array)
-  print(len(s_array))
-  for string in s_array:
-    string = first + string
-    print("string:")
-    print(string)
-    if string not in s:
-      continue
-    start = string.index(first) + len(first)
-    print("start:")
-    print(start)
-    try:
-      end = string.index(last, start)
-    except ValueError:
-      continue
-    print("end:")
-    print(end)
-    ret = string[start:end]
-    print("ret:")
-    print(ret)
-    if ret == '':
-      continue
-    if ret not in find_bw_array:
-      find_bw_array.append(ret)
-  return find_bw_array
-
-
+  
 def list_to_string(lst):
   string = "\""
   for index, item in enumerate(lst):
@@ -187,6 +188,7 @@ def gen_dict_entry(dsl_info):
   out_precision = dsl_info.get_out_precision()
   out_precision_string = "\"" + out_precision + "\""
   semantics = dsl_info.get_semantics()
+  semantics_string = "\"" + semantics + "\""
   return f'''
     "name" : {name},
     "args" : {args_string},
@@ -194,7 +196,7 @@ def gen_dict_entry(dsl_info):
     "size" : {size_string},
     "in_precision" : {in_precision_string},
     "out_precision" : {out_precision_string},
-    "semantics" : {semantics}, 
+    "semantics" : {semantics_string}, 
  '''
 
 def gen_tensor_dsl_dict(spec_list, dsl_file):
@@ -221,3 +223,6 @@ if __name__ == '__main__':
   for inst, instinfo in x86_sema.items():
     spec_list.append(instinfo['semantics'])
   gen_tensor_dsl_dict(spec_list, "test_tensor_dsl.py")
+
+
+
