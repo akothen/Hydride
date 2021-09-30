3661eab4d0c5a2304ae26e5d0150a515b3a831b2
Akash Kothari <akashk4@illinois.edu>
2021-09-30 16:30:18 -0500

    Update pattern_gen.py

--- new_legalizer_gen/pattern_gen.py
+++ new_legalizer_gen/pattern_gen.py
@@ -3,18 +3,16 @@
 from dslinfo import DSLInstInfo
 from utility import list_to_string
 
-from x86_sema import x86_sema
-
 
 def gen_inst_map_entry(inst_spec_map, inst_sema_map, inst_name):
   dsl_info = DSLInstInfo(inst_spec_map[inst_name])
   name = "\"" + inst_name + "\""
   args = dsl_info.get_args()
   args_string = list_to_string(args)
-  regs_string = inst_sema_map[inst_name]["args"]
-  size_string = inst_sema_map[inst_name]["bitwidth"]
-  in_precision_string = inst_sema_map[inst_name]["input_precision"]
-  out_precision_string = inst_sema_map[inst_name]["output_precision"]
+  regs_string = "\"" + inst_sema_map[inst_name]["args"] + "\""
+  size_string = "\"" + inst_sema_map[inst_name]["bitwidth"] + "\""
+  in_precision_string = "\"" + inst_sema_map[inst_name]["input_precision"] + "\""
+  out_precision_string = "\"" + inst_sema_map[inst_name]["output_precision"] + "\""
   semantics = dsl_info.get_semantics()
   semantics_string = "\"" + semantics + "\""
   return f'''
@@ -28,8 +26,8 @@ def gen_inst_map_entry(inst_spec_map, inst_sema_map, inst_name):
   '''
 
 
-def gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, intrisic_name, inst_list):
-  string = "\n" + intrisic_name +  " = {\n"
+def gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, inst_list):
+  string = ""
   for inst_name in inst_list:
     entry = gen_inst_map_entry(inst_spec_map, inst_sema_map, inst_name)
     name = "\"" + inst_name + "\""
@@ -43,12 +41,11 @@ def gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, intrisic_n
 def gen_intrinsic_target_inst_map(intrinsic_to_inst_map, inst_spec_map, inst_sema_map, legalizer_file):
   string = "\n\nintrinsics = {\n"
   for intrisic_name, inst_list in intrinsic_to_inst_map.items():
-    name = "\"" + intrisic_name + "\""
+    name = "\n\"" + intrisic_name +  "\""
     key = name + " : {\n"
-    entry = gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, intrisic_name, inst_list)
-    full_entry = key + entry + "},\n\n"
+    entry = gen_intrinsic_target_inst_map_entry(inst_spec_map, inst_sema_map, inst_list)
+    full_entry = key + entry + "\n},\n\n"
     string += full_entry
-  string += "}"
   f = open(legalizer_file, "w")
   f.write(string)
 
