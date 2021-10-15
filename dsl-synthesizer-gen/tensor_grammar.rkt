a696a20f339dedf82b54c57f5c3467955b213139
RafaeNoor <abdurrafae98@live.com>
2021-10-14 19:07:13 -0500

    commenting out multi concat

--- dsl-synthesizer-gen/tensor_grammar.rkt
+++ dsl-synthesizer-gen/tensor_grammar.rkt
@@ -6,7 +6,7 @@
 	(no-op (expr))
 	(bv 0 (bitvector 128))
 	(concat (expr) (expr))
-	(apply concat (list (expr) (expr) (expr) (expr)))
+	;(apply concat (list (expr) (expr) (expr) (expr)))
 	(ext-matrix-row (expr) 2 2 0 32)
 	(ext-matrix-row (expr) 2 2 1 32)
 	(ext-matrix-col (expr) 2 2 0 32)
