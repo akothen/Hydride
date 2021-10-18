7466974e6f1f43b9aa9040fbbd234d93cd6ddfa5
AkashIwnK <akashk4@illinois.edu>
2021-10-17 21:33:09 -0500

    Fix some files

--- dsl-synthesizer-gen/regtiling/tensor_grammar_32bit.rkt
+++ dsl-synthesizer-gen/regtiling/tensor_grammar_32bit.rkt
@@ -2,39 +2,22 @@
 (define-grammar (gen-grammar arg0 arg1)
 [top (choose
        (apply concat (list (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) ))
-       ;;(apply concat (list (expr) (expr) (expr) (expr)))
-       ;;(concat (top) (top))
 )]
 
 [expr (choose
 	arg0
 	arg1
 	(no-op (expr))
-	;;(bv 0 (bitvector 32))
-	;;(bv 0 (bitvector 96))
-	;;(bv 0 (bitvector 64))
 	(bv 0 (bitvector 128))
-	;;(concat (expr) (expr))
-    ;;(apply concat (list  (expr) (expr) (expr) (expr) (expr) (expr) ))
-    ;;(apply concat (list (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) (expr) ))
-	(dsl_inst_0 (bv 0 (bitvector 64)) (expr) (expr) 1 2 32 32)
-	;;(dsl_inst_1 (expr) 2 8)
-	(vector-load arg0 4 0 2 32)
-	(vector-load arg0 4 1 2 32)
-	;;(vector-load arg0 4 2 2 8)
-	;;(vector-load arg0 4 3 2 8)
-	(strided-gather arg1 16 0 4 2 32)
-	(strided-gather arg1 16 1 4 2 32)
-	;;(strided-gather arg1 16 2 4 2 8)
-
-	;;(ext-matrix-row arg0 2 4 0 8)
-	;;(ext-matrix-row arg0 2 4 1 8)
-	;;(ext-matrix-col arg1 4 6 0 8)
-	;;(ext-matrix-col arg1 4 6 1 8)
-	;;(ext-matrix-col arg1 4 6 2 8)
-	;;(ext-matrix-col arg1 4 6 3 8)
-	;;(ext-matrix-col arg1 4 6 4 8)
-	;;(ext-matrix-col arg1 4 6 5 8)
+        (dsl_inst_0 (expr) (expr) (expr) 1 2 32 32)
+	(ext-matrix-row (expr) 2 2 0 32)
+	(ext-matrix-row (expr) 2 2 1 32)
+	(ext-matrix-col (expr) 2 2 0 32)
+	(ext-matrix-col (expr) 2 2 1 32)
+	;;(vector-load arg0 4 0 2 32)
+	;;(vector-load arg0 4 1 2 32)
+	;;(strided-gather arg1 4 0 2 32)
+	;;(strided-gather arg1 4 1 2 32)
     )]
 )
 
