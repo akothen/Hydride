ead72d897cda237230280390a3a82ff14220a10f
Akash Kothari <akashk4@illinois.edu>
2021-10-21 01:42:05 -0500

    remove vector-mac from utils

--- dsl-synthesizer-gen/regtiling/racket_utils.rkt
+++ dsl-synthesizer-gen/regtiling/racket_utils.rkt
@@ -108,19 +108,6 @@
   result
 )
 
-;; An example of vector-mac instruction
-(define (vector-mac v1 v2 v3 num_elems type_size)
-  (define result
-   (apply
-    concat
-    (for/list ([i (reverse (range num_elems))])
-       (define tmp
-         (bvmul (ext-bv v2 i type_size) (ext-bv v3 i type_size)))
-      (bvadd (ext-bv v1 i type_size) tmp)
-      )))
-  result
-)
-
 ;; Implementation of a simple custom concat operation
 (define (vector-shufl-concat v1 v2 num_elems type_size)
  (define old_size (* num_elems type_size))
