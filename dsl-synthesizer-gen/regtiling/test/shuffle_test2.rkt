0bfab75f4026304e44f5db1d6c3ee5d663fff769
Akash Kothari <akashk4@illinois.edu>
2021-10-21 16:51:24 -0500

    format matmul spec impl

--- dsl-synthesizer-gen/regtiling/test/shuffle_test2.rkt
+++ dsl-synthesizer-gen/regtiling/test/shuffle_test2.rkt
@@ -121,6 +121,7 @@
   result)
 
 
+;; Code equivalent to matmul
 (define (spec arg0 arg1)
   (define row0 (vector-load arg0 64 0 4 8))
   (define row1 (vector-load arg0 64 4 4 8))
@@ -152,8 +153,26 @@
  )
 
 
-(define (tensor-matmul arg1 arg2)  (apply  concat  (for/list ([i (reverse (range 2))])  (apply concat  (for/list ([j (reverse (range 6))])  (apply bvadd (for/list ([k (reverse (range 4))])  (define idx_left (+ (* i 4) k)) (define idx_right (+ (* k 6) j))(define value1 (ext-bv arg1 idx_left 8)) (define value2 (ext-bv arg2 idx_right 8))  (bvmul value1 value2)  )  )  )  )  )  ) );
+;; Matmul spec
+(define (tensor-matmul arg1 arg2)
+  (apply
+   concat
+   (for/list ([i (reverse (range 2))])
+     (apply
+      concat
+      (for/list ([j (reverse (range 6))])
+        (apply
+         bvadd
+         (for/list ([k (reverse (range 4))])
+           (define idx_left (+ (* i 4) k))
+           (define idx_right (+ (* k 6) j))
+           (define value1 (ext-bv arg1 idx_left 8))
+           (define value2 (ext-bv arg2 idx_right 8))
+           (bvmul value1 value2)))))))
+)
 
+
+;; Function using shuffle instructions
 (define (sketch arg0 arg1)
   (define row0 (vector-load arg0 64 0 4 8))
   (define row1 (vector-load arg0 64 4 4 8))
