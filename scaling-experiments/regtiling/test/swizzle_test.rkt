cf7fa3bf21ae014eb64002a1124a271b4aeb02ef
Akash Kothari <akashk4@illinois.edu>
2021-12-06 12:58:50 -0600

    clean up test

--- scaling-experiments/regtiling/test/swizzle_test.rkt
+++ scaling-experiments/regtiling/test/swizzle_test.rkt
@@ -19,63 +19,27 @@
   var)
 
 
-(define (swizzle idx group_size fan_size rot_factor)
-  (define (rot n i)
-    (define rot_res
-     (modulo (+ i rot_factor) n)
-    )
-    (pretty-print "rot_res:")
-    (pretty-print rot_res)
-    rot_res
+(define (swizzle idx group_size dis_size rot_factor)
+  (define (rot rot_fact n i)
+     (modulo (+ i rot_fact) n)
   )
 
-  (define (GCD a b)
-   (define gcd
-    (cond
-         [(> a b) (GCD b (- a b))]
-         [(< a b) (GCD a (- b a))]
-         [else a])
-    )
-    gcd
-   )
-
-  (define (fan cf n i)
-    (define fan_res
-       (modulo (+ (* i cf) (floor (/ i (/ n (GCD cf n))))) n)
-    )
-    (pretty-print "fan_res:")
-    (pretty-print fan_res)
-    (pretty-print "(/ n (GCD cf n)):")
-    (pretty-print (/ n (GCD cf n)))
-    fan_res
+  (define (displace dis_factor n i)
+       (modulo (+ (* i dis_factor) (floor (/ i (/ n (gcd dis_factor n))))) n)
   )
 
-  (define (group gs func_res i)
-    (define group_res
-      (+ (* (floor (/ i gs)) gs) func_res)
-    )
-    (pretty-print "group_res:")
-    (pretty-print group_res)
-    group_res
+  (define (group gs group_idx i)
+      (+ (* (floor (/ i gs)) gs) group_idx)
   )
-
-   (define (fan_rot gs cf i)
-       (define fan_rot_res
-          (rot gs (fan cf gs (modulo i gs)))
-        )
-        (pretty-print "fan_rot_res:")
-        (pretty-print fan_rot_res)
-        fan_rot_res
-    )
   
-   (define result (group group_size (rot group_size (fan fan_size group_size (modulo idx group_size))) idx))
+   (define result (group group_size (rot rot_factor group_size (displace dis_size group_size (modulo idx group_size))) idx))
 
   result
  )
 
 
 ;; General version of swizzle with single input vector
-(define (vector-shuffle-swizzle1 v num_elems type_size group_size fan_size rot_factor)
+(define (vector-shuffle-swizzle1 v num_elems type_size group_size dis_size rot_factor)
 (pretty-print "vector-shuffle-swizzle1")
 (define result
        (apply
@@ -84,7 +48,7 @@
           (pretty-print "START SWIZZLED INDEX COMPUTE")
           (pretty-print "raw index:")
           (pretty-print (- (- num_elems 1) i))
-          (define swizzled-index (swizzle (- (- num_elems 1) i) group_size fan_size rot_factor))
+          (define swizzled-index (swizzle (- (- num_elems 1) i) group_size dis_size rot_factor))
           (pretty-print "swizzled-index")
           (pretty-print swizzled-index)
           (ext-bv v (- (- num_elems 1) swizzled-index) type_size)
