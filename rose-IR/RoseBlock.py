99bacfc189a9d2bee8d3a54176d36fcf825957e2
Akash Kothari <akashk4@illinois.edu>
2021-12-20 13:11:51 -0600

    Update RoseBlock.py

--- rose-IR/RoseBlock.py
+++ rose-IR/RoseBlock.py
@@ -4,17 +4,19 @@ from RoseFunctionCall import RoseFunctionCall
 
 # Definition of Block 
 # A block is a list of operations and function calls
+# Blocka sre contained in regions
 class RoseBlock:
-    def __init__(self, OpList : list):
+    def __init__(self, OpList : list, ParentRegion):
         # Sanity check
         for Op in OpList:
             assert(isinstance(Op, RoseOperation) 
             or isinstance(Op, RoseFunctionCall))
         self.OpList = OpList
+        self.ParentRegion = ParentRegion
         
     @staticmethod
-    def create(OpList : list):
-        return RoseBlock(OpList)
+    def create(OpList : list, ParentRegion):
+        return RoseBlock(OpList, ParentRegion)
 
     def getOps(self):
         return self.OpList
