2d551fdaa7b3225f515e7bd7fb034c6411735b30
Akash Kothari <akashk4@illinois.edu>
2021-12-20 12:20:39 -0600

    Fix RoseOperation

--- rose-IR/RoseOperation.py
+++ rose-IR/RoseOperation.py
@@ -5,27 +5,19 @@ from RoseOpcode import RoseOpcode
 # An operation in Rosette
 # An operation is either an instruction.
 # TODO: complete the definition
-class RoseOperation:
+class RoseOperation(RoseValue):
     def __init__(self, Opcode : RoseOpcode, Name : str, OperandList : list, ParentBlock):
         # Sanity check to see that the operand list is complete
         assert(Opcode.inputsAreValid(OperandList))
         self.Opcode = Opcode
         self.OperandList = OperandList
-        self.Value = RoseValue(Name, Opcode.getOutputType(OperandList))
+        # The result of an operation is a RoseValue
+        super().__init__(Name, Opcode.getOutputType(OperandList))
         self.ParentBlock = ParentBlock
     
     @staticmethod
     def create(Opcode : RoseOpcode, Name : str, OperandList : list, ParentBlock = None):
         return RoseOperation(Opcode, Name, OperandList, ParentBlock)
-
-    def getName(self):
-        return self.Value.getName()
-    
-    def getType(self):
-        return self.Value.getType()
-
-    def setName(self, Name):
-        self.Value = RoseValue(Name, self.Value.getType())
     
     def getOperand(self, Index):
         assert(Index < self.len(self.OperandList))
@@ -41,6 +33,5 @@ class RoseOperation:
             Op += (" " + Operand.getName())
         Op += ")"
         print(Op)
+  
 
-
-        
