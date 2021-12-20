e1f6da26f80e2f4ef7fafb7772bbcc59524c4c89
Akash Kothari <akashk4@illinois.edu>
2021-12-20 12:09:20 -0600

    Add Rose Operation definitiion

---
+++ rose-IR/RoseOperation.py
@@ -0,0 +1,43 @@
+from RoseType import RoseType
+from RoseValue import RoseValue
+from RoseOpcode import RoseOpcode
+
+# An operation in Rosette
+# An operation is either an instruction.
+# TODO: complete the definition
+class RoseOperation:
+    def __init__(self, Opcode : RoseOpcode, Name : str, OperandList : list, ParentBlock):
+        # Sanity check to see that the operand list is complete
+        assert(Opcode.inputsAreValid(OperandList))
+        self.Opcode = Opcode
+        self.OperandList = OperandList
+        self.Value = RoseValue(Name, Opcode.getOutputType(OperandList))
+        self.ParentBlock = ParentBlock
+    
+    @staticmethod
+    def create(Opcode : RoseOpcode, Name : str, OperandList : list, ParentBlock = None):
+        return RoseOperation(Opcode, Name, OperandList, ParentBlock)
+
+    def getName(self):
+        return self.Value.getName()
+
+    def setName(self, Name):
+        self.Value = RoseValue(Name, self.Value.getType())
+    
+    def getOperand(self, Index):
+        assert(Index < self.len(self.OperandList))
+        return self.OperandList[Index]
+    
+    def setOperand(self, Index, Operand):
+        assert(Index < self.len(self.OperandList))
+        self.OperandList[Index] = Operand
+
+    def print(self):
+        Op = "(define (" + self.Value.getName() + " (" + str(self.Opcode)
+        for Operand in self.OperandList:
+            Op += (" " + Operand.getName())
+        Op += ")"
+        print(Op)
+
+
+        
