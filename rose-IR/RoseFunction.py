81678a2a5a3411bdd0034ab5e61ce20f760dc721
Akash Kothari <akashk4@illinois.edu>
2021-12-22 11:00:14 -0600

    Add function to set return value name

--- rose-IR/RoseFunction.py
+++ rose-IR/RoseFunction.py
@@ -1,6 +1,7 @@
 from RoseType import RoseType
 from RoseRegion import RoseRegion
 
+
 # This is need to track the lower-level regions,
 # blocks, operations, etc. This does not inherit
 # any other class for now.
@@ -12,14 +13,14 @@ class RoseFunction(RoseRegion):
         super().__init__(ParentRegion, RegionList)
     
     @staticmethod
-    def create(Name : str, ArgsList : list, RetType : RoseType, RegionList : list, ParentRegion = None):
+    def create(Name : str, ArgsList : list, RetType : RoseType, RegionList : list, ParentRegion : RoseRegion):
         return RoseFunction(Name, ArgsList, RetType, RegionList, ParentRegion)
-  
+    
     # Empty function
     @staticmethod
     def create(Name : str, ArgsList : list, RetType : RoseType):
         return RoseFunction(Name, ArgsList, RetType, [], None)
-    
+
     def getNumArgs(self):
         return len(self.ArgsList)
     
@@ -37,6 +38,9 @@ class RoseFunction(RoseRegion):
 
     def isTopLevelFunction(self):
         return (self.getParent() == None)
+    
+    def setRetValName(self, Name):
+        self.ReturnValue = RoseValue(Name, self.ReturnValue.getType())
 
     def print(self):
         # Print function signature first
@@ -45,6 +49,8 @@ class RoseFunction(RoseRegion):
             Func_Sig += (" " + Arg.getName())
         Func_Sig += ")"
         print(Func_Sig)
-        # Pront regions in this function
+        # Print regions in this function
         super().print()
+        print(")")
+ 
 
