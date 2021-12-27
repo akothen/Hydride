8ef26810f87b6317085d9c9b46665aece277825d
Akash Kothari <akashk4@illinois.edu>
2021-12-27 14:30:44 -0600

    fix the roseclass class

--- rose-IR/RoseConstant.py
+++ rose-IR/RoseConstant.py
@@ -1,24 +1,21 @@
 from RoseType import RoseType
 from RoseValue import RoseValue
 
+
 # Constant value (integer and float) class
-class Constant(RoseValue):
-    def __init__(self, Value, Type : RoseType):
-        self.Val = Value
-        super().__init__("", Type)
-    
-    @staticmethod
-    def create(Value, Bitwidth = 32):
-        if isinstance(Value, int):
-            return Constant(Value, RoseType.getIntegerTy(Bitwidth))
-        if isinstance(Value, float):
-            return Constant(Value, RoseType.getFloatTy(Bitwidth))
-        exit()
-        assert(False)
+class RoseConstant(RoseValue):
+  def __init__(self, Value, Type : RoseType):
+      self.Val = Value
+      super().__init__("", Type)
+  
+  @staticmethod
+  def create(Value, Bitwidth : int):
+      if isinstance(Value, int):
+          return RoseConstant(Value, RoseType.getIntegerTy(Bitwidth))
+      exit()
 
-    def getValue(self):
-        return self.Val
-    
-    def print(self):
-        print(self.Val)
-
+  def getValue(self):
+      return self.Val
+  
+  def print(self):
+      print(self.Val)
