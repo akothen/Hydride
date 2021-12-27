e4fef62f19ba5943d065c767bd77843f19aa3d79
Akash Kothari <akashk4@illinois.edu>
2021-12-27 16:59:43 -0600

    Add RoseArgument class

---
+++ rose-IR/RoseArgument.py
@@ -0,0 +1,29 @@
+
+from RoseType import RoseType
+from RoseValue import RoseValue
+from RoseFunction import RoseFunction
+
+
+class RoseArgument(RoseValue):
+  def __init__(self, Name : str, Type : RoseType, Function : RoseFunction, ArgIndex : int):
+    # Some sanity checks
+    assert ArgIndex < Function.getNumArgs() 
+    self.Function = Function
+    self.ArgIndex = ArgIndex
+    super().__init__(Name, Type)
+  
+  @staticmethod
+  def create(Name : str, Type : RoseType, Function : RoseFunction = 0, ArgIndex : int = 0):
+    return RoseArgument(Name, Type, Function, ArgIndex)
+
+  def getArgIndex(self):
+    return self.ArgIndex
+  
+  def getFunction(self):
+    return self.Function
+  
+  def print(self):
+    print(self.Val)
+
+ 
+
