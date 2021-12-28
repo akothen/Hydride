5357f5821b2f719f8ba82982ae86d14aa89a30bc
Akash Kothari <akashk4@illinois.edu>
2021-12-27 22:00:15 -0600

    Add more functionality to RoseFunction class

--- rose-IR/RoseFunction.py
+++ rose-IR/RoseFunction.py
@@ -1,16 +1,25 @@
-from RoseType import RoseType
+from types import FunctionType
+from RoseValue import RoseValue
+from RoseType import RoseType, RoseFunctionType
 from RoseRegion import RoseRegion
+from RoseArgument import RoseArgument
 
 
 # This is need to track the lower-level regions,
 # blocks, operations, etc. This does not inherit
 # any other class for now.
-class RoseFunction(RoseRegion):
+# A function is a region and a value
+class RoseFunction(RoseValue, RoseRegion):
     def __init__(self, Name : str, ArgsList : list, RetType : RoseType, RegionList : list, ParentRegion : RoseRegion):
-        self.Name = Name
+        # Sanity checks
+        for Arg in ArgsList:
+            assert isinstance(Arg, RoseArgument)
+        self.ReturnValue = None
         self.ArgList = ArgsList
-        self.ReturnValue = RoseValue("", RetType)
-        super().__init__(ParentRegion, RegionList)
+        ArgTyList = [Arg.getType() for Arg in ArgsList]
+        FunctionType = RoseFunctionType.create(ArgTyList, RetType)
+        RoseValue.__init__(Name, FunctionType)
+        RoseRegion.__init__(ParentRegion, RegionList)
     
     @staticmethod
     def create(Name : str, ArgsList : list, RetType : RoseType, RegionList : list, ParentRegion : RoseRegion):
@@ -20,7 +29,20 @@ class RoseFunction(RoseRegion):
     @staticmethod
     def create(Name : str, ArgsList : list, RetType : RoseType):
         return RoseFunction(Name, ArgsList, RetType, [], None)
-
+    
+    # Empty function skeleton
+    @staticmethod
+    def create(Name : str, FunctionType : RoseFunctionType):
+        ArgTypeList = FunctionType.getArgList()
+        ArgsList = []
+        for ArgTy, ArgIndex in enumerate(ArgTypeList):
+            ArgsList.append(RoseArgument.create("arg", ArgTy, None, ArgIndex))
+        Function = RoseFunction(Name, ArgsList, FunctionType.getReturnType(), [], None)
+        for Arg, ArgIndex in enumerate(ArgsList):
+            Arg.setFunction(Function)
+            Function.addArg(Arg, ArgIndex)
+        return Function
+    
     def getNumArgs(self):
         return len(self.ArgsList)
     
@@ -30,27 +52,29 @@ class RoseFunction(RoseRegion):
     def getReturnValue(self):
         return self.ReturnValue
     
-    def getReturnTy(self):
-        return self.ReturnValue.getType()
-    
-    def addArg(self, NewArg):
-        self.ArgList.append(NewArg)
+    def addArg(self, NewArg : RoseValue, ArgIndex : int):
+        # Some sanity checks
+        assert ArgIndex < len(self.ArgList)
+        assert self.ArgList[ArgIndex].getType() == NewArg.getType()
+        Arg = RoseArgument.create(NewArg.getName(), NewArg.getType(), self, ArgIndex)
+        self.ArgList[ArgIndex] = Arg
+        # TODO: Replace uses as well
 
     def isTopLevelFunction(self):
         return (self.getParent() == None)
     
     def setRetValName(self, Name):
-        self.ReturnValue = RoseValue(Name, self.ReturnValue.getType())
+        self.ReturnValue = RoseValue(Name, self.getType().getReturnType())
 
     def print(self):
         # Print function signature first
-        Func_Sig = "(define (" + self.Name
+        Func_Sig = "(define (" + self.getName()
         for Arg in self.ArgList:
             Func_Sig += (" " + Arg.getName())
         Func_Sig += ")"
         print(Func_Sig)
         # Print regions in this function
-        super().print()
+        RoseRegion.print()
         print(")")
  
 
