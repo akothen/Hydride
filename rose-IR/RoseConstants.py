2a718add9b1f3c6a2fed677bf44e79b0617bede1
Akash Kothari <akashk4@illinois.edu>
2021-12-30 21:17:05 -0600

    Update and rename RoseConstant.py to RoseConstants.py

--- rose-IR/RoseConstants.py
+++ rose-IR/RoseConstants.py
@@ -1,3 +1,4 @@
+
 from RoseType import RoseType
 from RoseValue import RoseValue
 
@@ -6,12 +7,14 @@ from RoseValue import RoseValue
 class RoseConstant(RoseValue):
   def __init__(self, Value, Type : RoseType):
       self.Val = Value
-      super().__init__("", Type)
+      super().__init__(str(Value), Type)
   
   @staticmethod
   def create(Value, Bitwidth : int):
       if isinstance(Value, int):
           return RoseConstant(Value, RoseType.getIntegerTy(Bitwidth))
+      if isinstance(Value, float):
+          return RoseConstant(Value, RoseType.getFloatTy())
       exit()
 
   def getValue(self):
@@ -19,3 +22,17 @@ class RoseConstant(RoseValue):
   
   def print(self):
       print(self.Val)
+
+
+
+class RoseUndef(RoseValue):
+  def __init__(self):
+      super().__init__("undef", RoseType.getUndefTy())
+  
+  @staticmethod
+  def create():
+        return RoseUndef()
+  
+  def print(self):
+      print(self.getName())
+ 
