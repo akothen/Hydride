5e6b4b5ab99c61c3832c4f2f81acf1e4abd12613
Akash Kothari <akashk4@illinois.edu>
2021-12-30 21:21:52 -0600

    Add operator overloaders to RoseValue

--- rose-IR/RoseValue.py
+++ rose-IR/RoseValue.py
@@ -1,15 +1,27 @@
 from RoseType import RoseType
 
+
 # Base class for Rosette values. The values have name
 # and type. Keeping this simple for now.
 class RoseValue:
-    def __init__(self, Name : str, Type : RoseType):
+    def __init__(self, Name : str, Type : RoseType, SubClassData = None):
         self.Name = Name
         self.Type = Type
+        self.SubClassData = SubClassData
     
     @staticmethod
-    def create(Name : str, Type : RoseType):
-        return RoseValue(Name, Type)
+    def create(Name : str, Type : RoseType, SubClassData = None):
+        return RoseValue(Name, Type, SubClassData)
+    
+    def __eq__(self, Other):
+        assert isinstance(Other, RoseValue)
+        return self.Name == Other.Name and self.Type == Other.Type \
+            and self.SubClassData == Other.SubClassData
+
+    def __ne__(self, Other):
+        assert isinstance(Other, RoseValue)
+        return self.Name != Other.Name or self.Type != Other.Type \
+            or self.SubClassData != Other.SubClassData
 
     def getType(self):
         return self.Type
@@ -19,9 +31,8 @@ class RoseValue:
     
     def setName(self, Name : str):
         self.Name = Name
-
+    
     def print(self, Debug = False):
         print(self.Name)
         if Debug == True:
             self.Type.print()
-
