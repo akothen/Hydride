632df63bb892d8588782b2283f07960d3c749292
Akash Kothari <akashk4@illinois.edu>
2022-01-01 23:16:48 -0600

    Fix the RoseOperation constructor

--- rose-IR/RoseOperation.py
+++ rose-IR/RoseOperation.py
@@ -1,24 +1,21 @@
 from RoseValue import RoseValue
 from RoseOpcode import RoseOpcode
-from RoseConstants import RoseUndefRegion
+from RoseConstants import RoseUndefRegion, RoseUndefValue
 import RoseAbstractions 
 
 # An operation in Rosette
 # An operation is either an instruction.
 class RoseOperation(RoseValue):
     def __init__(self, Opcode : RoseOpcode, Name : str, OperandList : list, \
-                ParentBlock = RoseUndefRegion(), SubClassData = None):
+                ParentBlock = RoseUndefRegion()):
         if not isinstance(ParentBlock, RoseUndefRegion):
             assert isinstance(ParentBlock, RoseAbstractions.RoseBlock)
         print(OperandList)
-        OpSubClassData = {}
-        OpSubClassData["opcode"] = Opcode
-        OpSubClassData["operands"] = OperandList
-        OpSubClassData["parent"] = ParentBlock
-        OpSubClassData["subclassdata"] = SubClassData
+        self.Opcode = Opcode
+        self.OperandList = OperandList
+        self.ParentBlock = ParentBlock
         # The result of an operation is a RoseValue
-        Type = Opcode.getOutputType(OperandList)
-        RoseValue.__init__(self, Name, Type, OpSubClassData)
+        super().__init__(Name, Opcode.getOutputType(OperandList))
         # Sanity check to see that the operand list is complete
         self.assertValidationOfInputs()
     
@@ -26,35 +23,44 @@ class RoseOperation(RoseValue):
     def create(Opcode : RoseOpcode, Name : str, OperandList : list, ParentBlock = RoseUndefRegion()):
         return RoseOperation(Opcode, Name, OperandList, ParentBlock)
     
+    # Overridable function
     def assertValidationOfInputs(self):
         assert self.getOpcode().inputsAreValid(self.getOperands())
+    
+    def __eq__(self, Other):
+        if isinstance(Other, RoseUndefValue):
+            return False
+        assert isinstance(Other, RoseOperation)
+        return self.Opcode == Other.Opcode and self.OperandList == Other.OperandList \
+           and self.ParentBlock != Other.ParentBlock and super().__eq__(Other)
+
+    def __ne__(self, Other):
+        if isinstance(Other, RoseUndefValue):
+            return True
+        assert isinstance(Other, RoseOperation)
+        return self.Opcode != Other.Opcode or self.OperandList != Other.OperandList \
+            or self.ParentBlock != Other.ParentBlock or super().__ne__(Other)
 
     def getOpcode(self):
-        return self.getSubClassData()["opcode"]
+        return self.Opcode
     
     def getOperands(self):
-        return self.getSubClassData()["operands"]
+        return self.OperandList
     
     def getParent(self):
-        return self.getSubClassData()["parent"]
-    
-    def getOpSubClassData(self):
-        return self.getSubClassData()["subclassdata"]
+        return self.ParentBlock
     
     def getOperand(self, Index):
-        OperandList = self.getOperands()
-        assert Index < self.len(OperandList)
-        return OperandList[Index]
+        assert Index < self.len(self.OperandList)
+        return self.OperandList[Index]
     
     def setOperand(self, Index, Operand):
-        OperandList = self.getOperands()
-        assert Index < self.len(OperandList)
-        OperandList[Index] = Operand
-        self.setSubClassData(OperandList, "operands")
+        assert Index < self.len(self.OperandList)
+        self.OperandList[Index] = Operand
     
     def setParent(self, Block):
         assert isinstance(Block, RoseAbstractions.RoseBlock)
-        self.setSubClassData(Block, "operands")
+        self.ParentBlock = Block
 
     def print(self):
         Op = "(define (" + self.getName() + " (" + str(self.getOpcode())
