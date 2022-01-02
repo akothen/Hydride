d6860f71cd0aee04f3f3e48079b60c5b3c4e12ed
Akash Kothari <akashk4@illinois.edu>
2022-01-01 23:21:24 -0600

    Remove SubClassData in RoseValue

--- rose-IR/RoseValue.py
+++ rose-IR/RoseValue.py
@@ -4,40 +4,27 @@ from RoseType import RoseType
 # Base class for Rosette values. The values have name
 # and type. Keeping this simple for now.
 class RoseValue:
-    def __init__(self, Name : str, Type : RoseType, SubClassData = None):
+    def __init__(self, Name : str, Type : RoseType):
         self.Name = Name
         self.Type = Type
-        self.SubClassData = SubClassData
-        print(self.SubClassData)
     
     @staticmethod
-    def create(Name : str, Type : RoseType, SubClassData = None):
-        return RoseValue(Name, Type, SubClassData)
+    def create(Name : str, Type : RoseType):
+        return RoseValue(Name, Type)
     
     def __eq__(self, Other):
         assert isinstance(Other, RoseValue)
-        return self.Name == Other.Name and self.Type == Other.Type \
-            and self.SubClassData == Other.SubClassData
+        return self.Name == Other.Name and self.Type == Other.Type
 
     def __ne__(self, Other):
         assert isinstance(Other, RoseValue)
-        return self.Name != Other.Name or self.Type != Other.Type \
-            or self.SubClassData != Other.SubClassData
+        return self.Name != Other.Name or self.Type != Other.Type
 
     def getType(self):
         return self.Type
 
     def getName(self):
         return self.Name
-    
-    def getSubClassData(self):
-        return self.SubClassData
-    
-    def setSubClassData(self, Data, Key = None):
-        if Key == None:
-            self.SubClassData = Data
-        else:
-            self.SubClassData[Key] = Data
     
     def setName(self, Name : str):
         self.Name = Name
