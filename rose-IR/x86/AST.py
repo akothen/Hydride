709c55130431b49ff6db88f64bd0e4bf3ff299f1
Akash Kothari <akashk4@illinois.edu>
2022-01-05 00:49:59 -0600

    expr_id --> id

--- rose-IR/x86/AST.py
+++ rose-IR/x86/AST.py
@@ -1,29 +1,29 @@
 
 from collections import namedtuple
 
-BitSlice = namedtuple('BitSlice', ['bv', 'hi', 'lo', 'expr_id'])
-Var = namedtuple('Var', ['name', 'expr_id'])
+BitSlice = namedtuple('BitSlice', ['bv', 'hi', 'lo', 'id'])
+Var = namedtuple('Var', ['name', 'id'])
 Number = namedtuple('Number', ['val'])
 Update = namedtuple('Update', ['lhs', 'rhs'])
 # inc is a flag specifying whether we increment of decrement the induction variable
-For = namedtuple('For', ['iterator', 'begin', 'end', 'body', 'inc', 'expr_id'])
-While = namedtuple('While', ['cond', 'body', 'expr_id'])
-If = namedtuple('If', ['cond', 'then', 'otherwise', 'expr_id'])
-Call = namedtuple('Call', ['func', 'args', 'expr_id'])
-BinaryExpr = namedtuple('BinaryExpr', ['op', 'a', 'b', 'expr_id'])
-UnaryExpr = namedtuple('UnaryExpr', ['op', 'a', 'expr_id'])
+For = namedtuple('For', ['iterator', 'begin', 'end', 'body', 'inc', 'id'])
+While = namedtuple('While', ['cond', 'body', 'id'])
+If = namedtuple('If', ['cond', 'then', 'otherwise', 'id'])
+Call = namedtuple('Call', ['func', 'args', 'id'])
+BinaryExpr = namedtuple('BinaryExpr', ['op', 'a', 'b', 'id'])
+UnaryExpr = namedtuple('UnaryExpr', ['op', 'a', 'id'])
 Return = namedtuple('Return', ['val'])
-Select = namedtuple('Select', ['cond', 'then', 'otherwise', 'expr_id'])
-Match = namedtuple('Match', ['val', 'cases', 'expr_id'])
-Case = namedtuple('Case', ['val', 'stmts', 'expr_id'])
+Select = namedtuple('Select', ['cond', 'then', 'otherwise', 'id'])
+Match = namedtuple('Match', ['val', 'cases', 'id'])
+Case = namedtuple('Case', ['val', 'stmts', 'id'])
 # property lookup
 Lookup = namedtuple('Lookup', ['obj', 'key'])
 Index = namedtuple('Index', ['obj', 'idx'])
-FuncDef = namedtuple('FuncDef', ['name', 'params', 'body', 'expr_id'])
-#Arg = namedtuple('Arg', ['arg', 'expr_id'])
+FuncDef = namedtuple('FuncDef', ['name', 'params', 'body', 'id'])
+#Arg = namedtuple('Arg', ['arg', 'id'])
 Break = namedtuple('Break', [])
 
-Parameter = namedtuple('Parameter', ['name', 'type', 'is_signed', 'is_imm'])
+Parameter = namedtuple('Parameter', ['name', 'type', 'is_signed', 'is_imm', 'id'])
 Spec = namedtuple('Spec', [
   'intrin', 'inst', 'params',
   'spec', 'rettype', 'binary_exprs',
