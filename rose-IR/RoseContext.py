bc5f95c2e75e56b63d512eda1ce0247c3bbca7ff
Akash Kothari <akashk4@illinois.edu>
2022-01-05 23:42:16 -0600

    Major fixes to Rose Context class

--- rose-IR/RoseContext.py
+++ rose-IR/RoseContext.py
@@ -10,48 +10,61 @@ from RoseOperation import RoseOperation
 # This is a generic context that could be used across
 # different architectures.
 class RoseContext:
-  # Keep this simple
-  class RoseValueNameGenerator:
-    def __init__(self):
-      self.Num = 0
-    
-    def generateName(self):
-      Name =  "%" + str(self.Num)
-      str.Num += 1
-      return Name
-  
   def __init__(self):
-    # Keep track of all variables (arguments, LHS)
-    self.LhsVariables = {}
-    self.RhsOperations = {}
-    self.Variables = {}
-    # Cache the functions we encounter
-    self.FunctionDefs = {}
-    self.Functions = {}
+    self.CompiledAbstractions = dict()   # ID --> Some Rose abstraction
     # Track the contexts we encounter
     self.ParentContext = None
-    self.Contexts = {}
+    self.Contexts = {}   # ID --> child context
     # Heirarchical abstractions such as functions, loops and cond regions.
     # Blocks are not dealt with by this compiler.
-    self.RootAbstractions = []
-    # Every context has its own name generator
-    self.NameGenerator = self.RoseValueNameGenerator()
+    self.RootAbstractions = list()
+    # Variable names are associated with their IDs
+    self.Variables = {}    # Name --> ID
   
-  def getNewName(self):
-    Name = self.NameGenerator.generateName()
-    assert type(Name) == str
-    return Name
+  def isCompiledAbstraction(self, ID : str):
+    if ID in self.CompiledAbstractions:
+      return True
+    return False
   
-  def createContext(self, ContextName : str, ChildContext):
+  def addCompiledAbstraction(self, ID : str, Abstraction):
+    self.CompiledAbstractions[ID] = Abstraction
+  
+  def updateCompiledAbstraction(self, ID : str, NewAbstraction):
+    assert ID in self.CompiledAbstractions
+    self.CompiledAbstractions[ID] = NewAbstraction
+
+  def getCompiledAbstractionForID(self, ID : str):
+    assert ID in self.CompiledAbstractions
+    return self.CompiledAbstractions[ID]
+  
+  def getIDForCompiledAbstraction(self, Abstaction):
+    for ID, Value in self.CompiledAbstractions.items():
+      if Value == Abstaction:
+        return ID
+    assert False
+  
+  def addVariable(self, Name : str, ID : str):
+    self.Variables[Name] = ID
+  
+  def getVariableID(self, Name : str):
+    assert Name in self.Variables
+    return self.Variables[Name]
+  
+  def isVariableDefined(self, Name : str):
+    if Name in self.Variables:
+      return True
+    return False
+  
+  def createContext(self, ID : str, ChildContext):
     assert isinstance(ChildContext, RoseContext)
     ChildContext.setParentContext(self)
-    self.Contexts[ContextName] = ChildContext
+    self.Contexts[ID] = ChildContext
   
-  def destroyContext(self, ContextName : str):
-    self.Contexts[ContextName] = None
+  def destroyContext(self, ID : str):
+    self.Contexts[ID] = None
 
-  def getContext(self, AbstractionName : str):
-    return self.Contexts.get(AbstractionName, None)
+  def getContext(self, ID : str):
+    return self.Contexts.get(ID, None)
   
   def pushRootAbstraction(self, Abstraction):
     self.RootAbstractions.append(Abstraction)
@@ -61,23 +74,6 @@ class RoseContext:
   
   def getRootAbstraction(self):
     return self.RootAbstractions[len(self.RootAbstractions) - 1]
-  
-  def addFunctionDef(self, FunctionDef):
-    assert(type(FunctionDef) == FuncDef)
-    self.FunctionDefs[FunctionDef.name] = FunctionDef
-  
-  def getFunctionDef(self, Name : str):
-    return self.FunctionDefs[Name]
-
-  def addFunction(self, Function : RoseFunction):
-    Name = Function.getName()
-    self.Functions[Name] = Function
-
-  def getFunction(self, Name : str):
-    return self.Functions.get(Name, RoseUndefRegion())
-  
-  def addRhsOperation(self, Operation : RoseOperation):
-    self.RhsOperations.append(Operation)
   
   def setParentContext(self, Context):
     assert isinstance(Context, RoseContext)
@@ -89,27 +85,10 @@ class RoseContext:
   def isRootContext(self):
     return self.ParentContext == None
   
-  def copyVariables(self, Parent):
-    assert isinstance(Parent, RoseContext)
-    self.Variables = deepcopy(Parent.getVariables())
+  def getCompiledAbstractions(self):
+    return self.CompiledAbstractions
 
-  def getVariables(self):
-    return self.Variables
-
-  def addVariable(self, Value : RoseValue):
-    Name = Value.getName()
-    assert Name not in self.Variables
-    self.Variables[Name] = Value
-  
-  def isVariableDefined(self, Name : str):
-    return Name in self.Variables
-
-  def getVariableValue(self, Name : str):
-    return self.Variables[Name]
-
-  def setVariableValue(self, Name : str, Value : RoseValue):
-    self.Variables[Name] = Value
-  
-  def setVariableType(self, Name : str, Type : RoseType):
-    self.Variables[Name] = RoseValue.create(Name, Type)
-  
+  def copyAbstractionsFromParent(self):
+    assert not self.isRootContext()
+    assert isinstance(self.ParentContext, RoseContext)
+    self.CompiledAbstractions = deepcopy(self.Parent.getCompiledAbstractions())
