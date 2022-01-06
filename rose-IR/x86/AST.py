72fd4e65e108cdef8371e6e10d25a1ccd7b464fb
Akash Kothari <akashk4@illinois.edu>
2022-01-05 23:34:55 -0600

    Reorganize statements a bit and fix bug

--- rose-IR/x86/AST.py
+++ rose-IR/x86/AST.py
@@ -1,26 +1,33 @@
 
 from collections import namedtuple
 
+Number = namedtuple('Number', ['val'])
 BitSlice = namedtuple('BitSlice', ['bv', 'hi', 'lo', 'id'])
 Var = namedtuple('Var', ['name', 'id'])
-Number = namedtuple('Number', ['val'])
+
 Update = namedtuple('Update', ['lhs', 'rhs'])
-# inc is a flag specifying whether we increment of decrement the induction variable
+
 For = namedtuple('For', ['iterator', 'begin', 'end', 'body', 'inc', 'id'])
 While = namedtuple('While', ['cond', 'body', 'id'])
+
 If = namedtuple('If', ['cond', 'then', 'otherwise', 'id'])
+
 Call = namedtuple('Call', ['func', 'args', 'id'])
+
 BinaryExpr = namedtuple('BinaryExpr', ['op', 'a', 'b', 'id'])
 UnaryExpr = namedtuple('UnaryExpr', ['op', 'a', 'id'])
-Return = namedtuple('Return', ['val'])
+
+FuncDef = namedtuple('FuncDef', ['name', 'params', 'body', 'id'])
+Return = namedtuple('Return', ['val', 'id'])
+
 Select = namedtuple('Select', ['cond', 'then', 'otherwise', 'id'])
+
 Match = namedtuple('Match', ['val', 'cases', 'id'])
 Case = namedtuple('Case', ['val', 'stmts', 'id'])
-# property lookup
+
 Lookup = namedtuple('Lookup', ['obj', 'key'])
 Index = namedtuple('Index', ['obj', 'idx'])
-FuncDef = namedtuple('FuncDef', ['name', 'params', 'body', 'id'])
-#Arg = namedtuple('Arg', ['arg', 'id'])
+
 Break = namedtuple('Break', [])
 
 Parameter = namedtuple('Parameter', ['name', 'type', 'is_signed', 'is_imm', 'id'])
