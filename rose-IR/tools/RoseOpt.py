0fad35ef336b484520b67e6a4a6d122ee418bdba
Akash Kothari <akashk4@illinois.edu>
2022-01-13 04:19:05 -0600

    Add more transformations to be pipeline

--- rose-IR/tools/RoseOpt.py
+++ rose-IR/tools/RoseOpt.py
@@ -3,13 +3,15 @@ import x86RoseLang
 
 import RoseCSE
 import RoseLoopReroller
-
+import RoseFunctionInliner
 
 
 if __name__ == '__main__':
   Function = x86RoseLang.Compile()
   RoseCSE.Run(Function)
   RoseLoopReroller.Run(Function)
+  RoseCSE.Run(Function)
+  RoseFunctionInliner.Run(Function)
 
 
 
