6607f6d0ab53b93a7db0bdb25882e052dc8c7e71
Akash Kothari <akashk4@illinois.edu>
2022-01-28 03:07:29 -0600

    Re-add accidentally removed single input swizzle

--- dsl-synthesizer-gen/regtiling/racket_utils.rkt
+++ dsl-synthesizer-gen/regtiling/racket_utils.rkt
@@ -342,6 +342,41 @@
   result
 )
 
+
+(define (swizzle idx group_size dis_size rot_factor)
+  (define (rot rot_fact n i)
+     (modulo (+ i rot_fact) n)
+  )
+
+  (define (displace dis_factor n i)
+       (modulo (+ (* i dis_factor) (floor (/ i (/ n (gcd dis_factor n))))) n)
+  )
+
+  (define (group gs group_idx i)
+      (+ (* (floor (/ i gs)) gs) group_idx)
+  )
+
+  (define result (group group_size (rot rot_factor group_size (displace dis_size group_size (modulo idx group_size))) idx))
+
+  result
+ )
+
+
+;; General version of swizzle with single input vector
+(define (vector-single-input-swizzle v num_elems type_size group_size dis_size rot_factor)
+  (define result
+      (apply
+      concat
+      (for/list ([i (reverse (range num_elems))])
+        (define swizzled-index (swizzle (- (- num_elems 1) i) group_size dis_size rot_factor))
+        (ext-bv v (- (- num_elems 1) swizzled-index) type_size)
+        )
+      )
+  )
+  result
+)
+
+
 (define (print-vector vec len precision)
   (for/list ( [i (reverse (range len))])
             (define ith_val (ext-bv vec i precision))
