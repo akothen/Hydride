2de86d9a1fcc450ad663a23dc4d86c8d1425f4dd
Akash Kothari <akashk4@illinois.edu>
2022-02-10 18:03:01 -0600

    Add a rose canonicalizing pass

---
+++ compiler-generator/similarity-checker/RoseCanonicalize.py
@@ -0,0 +1,158 @@
+#############################################################
+#
+# Changes the loop bounds to canonical form.
+#
+#############################################################
+
+
+from pickle import TRUE
+from RoseType import RoseType
+from RoseValue import RoseValue
+from RoseAbstractions import *
+from RoseValues import *
+from RoseOperations import *
+from RoseBitVectorOperations import *
+from RoseUtilities import *
+from RoseSimilarityCheckerUtilities import *
+
+
+def RunFixLoopsBooundsOnLoop(Loop : RoseForLoop):
+  print("FIX BOUNDS OF LOOP")
+  print("LOOP:")
+  print(Loop)
+  Loop.print()
+  FunctionOutput = Loop.getFunction().getReturnValue()
+  BVInsertOps = []
+  # Get the bvinserts to the output of the function that this belongs to.
+  for Region in Loop:
+    if isinstance(Region, RoseBlock):
+      for Op in Region:
+        if isinstance(Op, RoseBVInsertSliceOp):
+          if Op.getInputBitVector() == FunctionOutput:
+            BVInsertOps.append(Op)
+    
+  # Go over the bvinserts and see if the bitwidth and loop step are the same.
+  # But first make sure the bitwidth for all bvinserts is the same.
+  BitWidth = BVInsertOps[0].getOutputBitwidth()
+  for Op in BVInsertOps:
+    assert Op.getOutputBitwidth() == BitWidth
+  # Now see if the loop bounds need adjusting
+  LoopStep = Loop.getStep()
+  assert isinstance(LoopStep, RoseConstant)
+  if BitWidth != LoopStep.getValue():
+    # Compute factor
+    assert LoopStep.getValue() < BitWidth
+    Factor = BitWidth / LoopStep.getValue()
+    assert Factor == int(Factor)
+    Factor = int(Factor)
+    # Assuming loop start and end are constant values as well.
+    Start = Loop.getStartIndex()
+    End = Loop.getEndIndex()
+    assert isinstance(Start, RoseConstant)
+    assert isinstance(End, RoseConstant)
+    # Fix the loop bounds now
+    Loop.setStartIndex(Factor * Start.getValue())
+    Loop.setEndIndex(Factor * End.getValue())
+    Loop.setStep(Factor * LoopStep.getValue())
+    # Now change the iterator name
+    OldIterator = Loop.getIterator()
+    Loop.setIteratorName(OldIterator.getName() + ".new")
+    # Replace the old iterator value with new one
+    NewIterator = Loop.getIterator()
+    FactorVal = RoseConstant(Factor, NewIterator.getType())
+    IteratorReplacement = RoseDivOp.create(OldIterator.getName() + ".new.div", \
+                             NewIterator, FactorVal)
+    # Check if the loop's first child region is a block, if not we insert one
+    if not isinstance(Loop.getChild(0), RoseBlock):
+      Block = RoseBlock.create([IteratorReplacement])
+      Loop.addRegionBefore(0, Block)
+    else:
+      Block = Loop.getChild(0)
+      Block.addOperationBefore(IteratorReplacement, Block.getOperation(0))
+    Loop.replaceUsesWith(OldIterator, IteratorReplacement)
+  print("NEW LOOP:")
+  Loop.print()
+
+
+def ExtractConstantsFromRegion(Region):
+  # Iterate over all the contents of this function
+  for Abstraction in Region:
+    # We do not inline functions in this pass.
+    assert isinstance(Abstraction, RoseFunction)
+    if isinstance(Abstraction, RoseForLoop):
+      RunFixLoopsBooundsOnLoop(Abstraction)
+      continue
+    # Skip blocks
+    if isinstance(Abstraction, RoseBlock):
+      continue
+    print("REGION:")
+    print(Abstraction)
+    Abstraction.print()
+    ExtractConstantsFromRegion(Abstraction)
+
+
+
+def AddOuterLoopInFunction(Function : RoseFunction):
+  # Use the return value of the function as the end and step
+  Bitwidth = Function.getReturnValue().getType().getBitwidth()
+  # Create a new loop
+  Loop = RoseForLoop.create("%" + "outer.it", 0, Bitwidth, Bitwidth)
+  # Add the regions in the function into the loop
+  RegionList = list()
+  for Region in Function:
+    RegionList.append(Region)
+  # The last region has the return operation
+  LastRegion = RegionList[len(RegionList) - 1]
+  ReturnOpFound = False
+  for Op in LastRegion:
+    if isinstance(Op, RoseReturnOp):
+      ReturnOpFound = True
+      break
+  assert ReturnOpFound == True
+  RegionList.pop(len(RegionList) - 1)
+  for Region in RegionList:
+    Function.eraseChild(Region)
+    Loop.addRegion(Region)
+  # Now add the loop to the function
+  Function.addRegionBefore(0, Loop)
+
+
+def CanonicalizeFunction(Function : RoseFunction):
+  print("CANONICALIZING FUNCTION")
+  print("FUNCTION:")
+  Function.print()
+  # See if the function is already canonicalize
+  if IsFunctionInCanonicalForm(Function) == True:
+    print("FUNCTION IS IN CANONICAL FORM")
+    return
+
+  # Adjust the loop bounds
+  print("ADJUST LOOP BOUNDS IN FUNCTION")
+  AdjustLoopBoundsInFunction(Function)
+  if IsFunctionInCanonicalForm(Function) == True:
+    print("_____FUNCTION IS IN CANONICAL FORM")
+    return
+
+  # There should be only one high-level loop
+  #if Function.numLevelsOfRegion(RoseForLoop, Level=0) != 1:
+    # Need to add a loop at the high level -- this is a very common case
+  AddOuterLoopInFunction(Function)
+  if IsFunctionInCanonicalForm(Function):
+    return
+
+  InvariantsMap = GetInvariantsInRegion(Function)
+  print(InvariantsMap)
+  for Key, Val in InvariantsMap.items():
+    print("--------")
+    print(Key)
+    print(Val)
+
+
+# Runs a transformation
+def Run(Function : RoseFunction):
+  CanonicalizeFunction(Function)
+  print("\n\n\n\n\n")
+  Function.print()
+
+
+
