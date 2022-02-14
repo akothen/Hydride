aabd1b70f2208504e8026aba3456194e5486adc4
AkashIwnK <akashk4@illinois.edu>
2022-02-14 02:34:33 -0600

    Fix bug in code that removes redundant bvinserts

--- compiler-generator/transform/RoseDCE.py
+++ compiler-generator/transform/RoseDCE.py
@@ -17,33 +17,76 @@ from RoseBitVectorOperations import *
 def RemoveRedundantBVInsertOps(Block : RoseBlock):
   BVInsertOpList = list()
   BVInsertToExtractMap = dict()
+  BVInsertIsNotDeletable = set()
   for Operation in Block:
     if isinstance(Operation, RoseBVInsertSliceOp):
       # If this op is already in the map, we rempve the entry
       # from the map. TODO: Imoprove this, if neessary.
-      if Operation in BVInsertToExtractMap:
-        BVInsertToExtractMap.pop(Operation)
-      BVInsertOpList.append(Operation)
+      for BVInsertOp, _ in BVInsertToExtractMap.items():
+        if BVInsertOp.getInputBitVector() == Operation.getInputBitVector():
+          BVInsertToExtractMap.pop(Operation)
+          break
+      # We only deal with cases where the indices are constants
+      BVInsertOpLow = Operation.getLowIndex()
+      BVInsertOpHigh= Operation.getHighIndex()
+      if isinstance(BVInsertOpLow, RoseConstant) \
+        and isinstance(BVInsertOpHigh, RoseConstant):
+        BVInsertOpList.append(Operation)
       continue
     if isinstance(Operation, RoseBVExtractSliceOp):
       for BVInsertOp in BVInsertOpList:
         if BVInsertOp.getInputBitVector() == Operation.getInputBitVector():
-          if Operation.getInputBitVector().getType().getBitwidth() \
-                == BVInsertOp.getInsertValue().getType().getBitwidth():
-            if BVInsertOp not in BVInsertToExtractMap:
-              BVInsertToExtractMap[BVInsertOp] = [Operation]
+          if Operation.getType().getBitwidth() \
+                <= BVInsertOp.getInsertValue().getType().getBitwidth():
+            # We only deal with cases where the indices are constants
+            BVExtractOpLow = Operation.getLowIndex()
+            BVExtractOpHigh = Operation.getHighIndex()
+            if isinstance(BVExtractOpLow, RoseConstant) \
+              and isinstance(BVExtractOpHigh, RoseConstant):
+              if BVInsertOp not in BVInsertToExtractMap:
+                BVInsertToExtractMap[BVInsertOp] = [Operation]
+              else:
+                BVInsertToExtractMap[BVInsertOp].append(Operation)
+              break
             else:
-              BVInsertToExtractMap[BVInsertOp].append(Operation)
-            break
+              BVInsertIsNotDeletable.add(BVInsertOp)
+          else:
+            BVInsertIsNotDeletable.add(BVInsertOp)
   
   # Now we replace the bvextracts with shift sand remove bvinserts.
   for BVInsertOp, BVExtractList in BVInsertToExtractMap.items():
     # Now we replace the bitvector being extracted from with the inserted
     # value to make the extraction more direct.
+    BVInsertOpLow = BVInsertOp.getLowIndex()
+    BVInsertOpHigh = BVInsertOp.getHighIndex()
     for BVExtractOp in BVExtractList:
+      assert BVInsertOp.getOutputBitwidth() >= BVExtractOp.getOutputBitwidth()
+      BVExtractOpLow = BVExtractOp.getLowIndex()
+      BVExtractOpHigh = BVExtractOp.getHighIndex()
+      # Compute the new low index
+      if BVExtractOpLow.getValue() >= BVInsertOpLow.getValue():
+        NewLow = RoseConstant.create(BVExtractOpLow.getValue() - BVInsertOpLow.getValue(), \
+                                    BVExtractOpLow.getType())
+      else:
+        # Cannot do much with this extract op
+        BVInsertIsNotDeletable.add(BVInsertOp)
+        continue
+      # Compute the new high index
+      if BVExtractOpLow.getValue() + BVExtractOp.getOutputBitwidth() \
+            <= BVInsertOpLow.getValue() + BVInsertOp.getOutputBitwidth():
+        NewHigh = RoseConstant.create(NewLow.getValue() + BVExtractOp.getOutputBitwidth(),\
+                                  BVExtractOpHigh.getType())
+      else:
+        # Cannot do much with this extract op
+        BVInsertIsNotDeletable.add(BVInsertOp)
+        continue
       BVExtractOp.setOperand(0, BVInsertOp.getInsertValue())
-    # Remove the bvinsert op from the block
-    Block.eraseOperation(BVInsertOp)
+      BVExtractOp.setOperand(1, NewLow)
+      BVExtractOp.setOperand(2, NewHigh)
+    
+    # Remove the bvinsert op from the block if we are done with all its uses
+    if BVInsertOp not in BVInsertIsNotDeletable:
+      Block.eraseOperation(BVInsertOp)
 
 
 def RunDCEOnBlock(Block : RoseBlock):
