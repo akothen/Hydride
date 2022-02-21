a7aba8636eb9a949cd66df527786463c352a840f
Akash Kothari <akashk4@illinois.edu>
2022-02-21 07:10:35 -0600

    Add algo for slides

---
+++ compiler-generator/examples/algo.py
@@ -0,0 +1,50 @@
+BVValToBitwidthVal = map()
+UnknownVal = list()
+for Op in reversed(Block):
+  if Op.isSizeChangingOp():
+    if Op in BVValToBitwidthVal:
+      if Op.getOperand() not in BVValToBitwidthVal:
+        UnknownVal.append(Op.getOperand())
+      continue
+    Arg = ExtractConstant(Op.sizeOperand())
+    BVValToBitwidthVal[Op] = Arg
+    continue
+  
+  if Op.outputAndInputTypesAresSame():
+    if Op in BVValToBitwidthVal:
+      for Operand in Op.getOperands():
+        BVValToBitwidthVal[Operand] = BVValToBitwidthVal[Op]
+    else:
+      for Operand in Op.getOperands():
+        UnknownVal.append(Operand)
+    continue
+
+  if Op.getOpcode() == RoseBVExtractOp:
+    if Op in BVValToBitwidthVal:
+      Op.setPrecision(BVValToBitwidthVal[Op])
+    else:
+      Arg = ExtractConstant(Op.sizeOperand())
+      BVValToBitwidthVal[Op] = Arg
+    # high_index = low_index + (precision - 1)
+    if Op.LowIndex().isConstant()
+      ExtractConstant(Op.LowIndex())
+    else:
+      assert Op.HighIndex().getOpcode() == RoseAddOp
+      LastIdx = RoseSubOp.create(Op.getPrecision(), 1)
+      Op.HighIndex().setOperand(LastIdx)
+    continue
+
+  if Op.getOpcode() == RoseBVInsertOp:
+    if Op.LowIndex() == LaneIndex:
+      Op.setPrecision(lane_size)
+      BVValToBitwidthVal[Op.getInsertVal()] = lane_size
+    else:
+      Op.setPrecision(elem_size)
+      BVValToBitwidthVal[Op.getInsertVal()] = elem_size
+    # high_index = low_index + (precision - 1)
+    if Op.LowIndex().isConstant()
+      ExtractConstant(Op.LowIndex())
+    else:
+      assert Op.HighIndex().getOpcode() == RoseAddOp
+      LastIdx = RoseSubOp.create(Op.getPrecision(), 1)
+      Op.HighIndex().setOperand(LastIdx)
