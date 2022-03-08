0cf2ee85e3e5b1506d05a4fb06e6f5079e216597
Akash Kothari <akashk4@illinois.edu>
2022-03-08 16:08:46 -0600

    Refactor some code out for rose code emitter

--- compiler-generator/tools/fuzzer/RoseCodeEmitter.py
+++ compiler-generator/tools/fuzzer/RoseCodeEmitter.py
@@ -1,26 +1,8 @@
 
 import subprocess
-from enum import Enum
 from copy import deepcopy
 
 from RoseAbstractions import RoseFunction
-
-
-def RunProcess(cmd):
-  try:
-    sproc = subprocess.Popen(cmd,
-                            shell=True,
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    stdout_data, stderr_data = sproc.communicate(timeout=60)
-    stdout_data = stdout_data.decode("utf-8", "ignore")
-    stderr_data = stderr_data.decode("utf-8", "ignore")
-    return stdout_data, stderr_data
-  except OSError:
-    return "", "OS Error running {}".format(cmd)
-  except subprocess.TimeoutExpired:
-    return "", "Timout Error running {}".format(cmd)
 
 
 def SizeInBytes(Size):
@@ -29,14 +11,6 @@ def SizeInBytes(Size):
   Numbytes = int(Size / 8)
   assert Numbytes * 8 == Size
   return Numbytes
-
-
-class ERR(Enum):
-  NONE = 0
-  NEQ = 1
-  ROSE = 2
-  C = 3
-  BOTH = 4
 
 
 class RoseCodeEmitter():
@@ -73,6 +47,7 @@ class RoseCodeEmitter():
     SOut, Srr = self.compile()
     if SOut is None and Srr is None:
       SOut, Srr = self.execute()
+      print(SOut)
     return SOut, Srr
 
   def createFile(self, args):
@@ -98,55 +73,20 @@ class RoseCodeEmitter():
     except IOError:
       print("Error making: {}".format(FileName))
       return False
-    
-  def execute(self):
-    NotImplemented
 
   def run(self, Cmd : str):
-    return RunProcess(Cmd)
-
-  def checkResult(self, RosetteOut, RosetteErr, COut, CErr):
-    Rose_Err = False
-    C_Err = False
-    if (RosetteErr is not None) and (RosetteErr != ""):
-        Rose_Err = True
-    if (CErr is not None) and (CErr != ""):
-        C_Err = True
-    if C_Err and Rose_Err:
-        return ERR.BOTH
-    if C_Err:
-        return ERR.C
-    if Rose_Err:
-        return ERR.ROSE
-    if COut != RosetteOut:
-        return ERR.NEQ
-    return ERR.NONE
-
-  def writeOutput(self, RosetteOut, RosetteOut_cmp, RosetteErr, COut, COutCmp, CErr, Result):
-    OutFile = "{}/{}".format(".", self.getFunction().getName())
     try:
-      File = open(OutFile, "w+")
-      FmtOut = "-----------------------------\n"
-      FmtOut += "{} - STDOUT:\n{}\n"
-      FmtOut += "-----------------------------\n"
-      FmtOut += "{} - CMPOUT:\n{}\n"
-      FmtOut += "-----------------------------\n"
-      FmtOut += "{} - STDERR:\n{}\n"
-      FmtOut += "-----------------------------\n\n"
-      Rose_Out = FmtOut.format("Rosette", RosetteOut, "Rosette", RosetteOut_cmp,
-                                "Rosette", RosetteErr)
-      C_Out = FmtOut.format("C", COut, "C", COutCmp, "C", CErr)
-      Res = "Result: " + str(Result) + "\n"
-      File.write(Rose_Out)
-      File.write(C_Out)
-      File.write(Res)
-      File.close()
-    except IOError:
-      print("Error writing output to: {}".format(OutFile))
-
-  def testResult(self, RosetteOut, RosetteOut_cmp, RosetteErr, COut, COutCmp, CErr):
-    Result = self.checkResult(RosetteOut_cmp, RosetteErr, COutCmp, CErr)
-    self.writeOutput(RosetteOut, RosetteOut_cmp, RosetteErr, COut, COutCmp, CErr, Result)
-    return Result
-
+      sproc = subprocess.Popen(Cmd,
+                              shell=True,
+                              stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE)
+      stdout_data, stderr_data = sproc.communicate(timeout=60)
+      stdout_data = stdout_data.decode("utf-8", "ignore")
+      stderr_data = stderr_data.decode("utf-8", "ignore")
+      return stdout_data, stderr_data
+    except OSError:
+      return "", "OS Error running {}".format(Cmd)
+    except subprocess.TimeoutExpired:
+      return "", "Timout Error running {}".format(Cmd)
 
