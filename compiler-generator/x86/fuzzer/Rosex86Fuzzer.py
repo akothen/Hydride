a6ee655e3ad6cbadf8910e73f918b68e3a1d29a3
Akash Kothari <akashk4@illinois.edu>
2022-03-08 13:29:56 -0600

    fix code

--- compiler-generator/x86/fuzzer/Rosex86Fuzzer.py
+++ compiler-generator/x86/fuzzer/Rosex86Fuzzer.py
@@ -60,26 +60,6 @@ class CCodeEmitter(RoseCodeEmitter):
   def getFileName(self):
     return "c_{}.c".format(self.getFunction().getName())
 
-  def c_make_val_i(self, Index, Param, ConcArgs):
-    BinOut = []
-    ParamBytes = SizeInBytes(Param.getType().getBitwidth())
-    for j in range(0, ParamBytes):
-      v = ConcArgs[Index][j] & 0xff
-      if self.getInstInfo().params[Index].is_imm:
-        #v = int(v % imm8_max(Function.getName()))
-        v = int(v & (2**(self.getInstInfo().imm_width) - 1))
-      BinOut.append(str(hex(v)))
-
-    if self.getInstInfo().params[Index].is_imm:
-      return "#define {} {}".format(Param.getName(), BinOut[0])
-    
-    Buf = "uint8_t _{}[{}] = LBRACK {} RBRACK;\n".format(Index, ParamBytes, ",".join(BinOut))
-    Buf = Buf.replace("LBRACK", "{")
-    Buf = Buf.replace("RBRACK", "}")
-    Var = "{} {};\n".format(x86ToC(self.getInstInfo().params[Index].type), Param.getName())
-    Init = "memcpy(&{}, _{}, {});\n".format(Param.getName(), Index, ParamBytes)
-    return Buf + Var + Init
-
   def createFile(self, ConcArgs : list):
     Content = ["#include <immintrin.h>\n", "#include <stdio.h>", \
                "#include <stdlib.h>", "#include <stdint.h>\n"]
@@ -89,11 +69,30 @@ class CCodeEmitter(RoseCodeEmitter):
     }\n\n
 }''')
     Content.append("int main() {")
+
+    def GenMainFunction(Index, Param, ConcArgs):
+      BinOut = []
+      ParamBytes = SizeInBytes(Param.getType().getBitwidth())
+      for j in range(0, ParamBytes):
+        v = ConcArgs[Index][j] & 0xff
+        if self.getInstInfo().params[Index].is_imm:
+          #v = int(v % imm8_max(Function.getName()))
+          v = int(v & (2**(self.getInstInfo().imm_width) - 1))
+        BinOut.append(str(hex(v)))
+      if self.getInstInfo().params[Index].is_imm:
+        return "#define {} {}".format(Param.getName(), BinOut[0])
+      Buf = "uint8_t _{}[{}] = LBRACK {} RBRACK;\n".format(Index, ParamBytes, ",".join(BinOut))
+      Buf = Buf.replace("LBRACK", "{")
+      Buf = Buf.replace("RBRACK", "}")
+      Var = "{} {};\n".format(x86ToC(self.getInstInfo().params[Index].type), Param.getName())
+      Init = "memcpy(&{}, _{}, {});\n".format(Param.getName(), Index, ParamBytes)
+      return Buf + Var + Init
+    
     Function = self.getFunction()
     Params = []
     for Index, Param in enumerate(self.getFunction().getArgs()):
-        Params.append(Param.getName())
-        Content.append(self.c_make_val_i(Index, Param, ConcArgs))
+      Params.append(Param.getName())
+      Content.append(GenMainFunction(Index, Param, ConcArgs))
     RetBitwidth = Function.getReturnValue().getType().getBitwidth()
     Content.append("uint8_t out[" + str(SizeInBytes(RetBitwidth)) + "] = {0};")
     Content.append("{} ret = {}({});".format(x86ToC(self.getInstInfo().rettype),
@@ -113,10 +112,6 @@ class CCodeEmitter(RoseCodeEmitter):
 
   def execute(self):
     return self.run("./c-exec")
-
-  def cleanOutput(self, out):
-    out = out.lstrip("0")
-    return out.lstrip().rstrip().zfill(self.intrin.ret.sz_bytes() * 2)
 
 
 if __name__ == '__main__':
