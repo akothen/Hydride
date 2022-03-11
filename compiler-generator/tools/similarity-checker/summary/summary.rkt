5bb1d873612b1cf560b4ff7616c7b3ddcd66ff9b
Akash Kothari <akashk4@illinois.edu>
2022-03-11 14:45:10 -0600

    Update summary generated by similarity checker

--- compiler-generator/tools/similarity-checker/summary/summary.rkt
+++ compiler-generator/tools/similarity-checker/summary/summary.rkt
@@ -1,11 +1,11 @@
-(define (_mm256_maskz_mov_epi16  k a %vectsize %lanesize %elemsize %laneoffset %arg0 )
+(define (_mm_maskz_mov_epi16  k a %vectsize %lanesize %elemsize %laneoffset %arg0 %arg1 )
 (apply
 concat
 (for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
  (apply
  concat
  (for/list ([j0.new (reverse (range %laneoffset %lanesize %elemsize))])
-  (define j0.new.div (/  j0.new  %elemsize))
+  (define j0.new.div (/  j0.new  %arg0))
   (define %1 (extract  j0.new.div j0.new.div k))
   (if (equal? %1 (bv #b1 1))
    (begin
@@ -17,7 +17,7 @@ concat
    (begin
    (define %lastidx1 (-  %elemsize  1))
    (define %7 (+  j0.new  %lastidx1))
-   %arg0
+   %arg1
    )
   )
  )
@@ -26,14 +26,14 @@ concat
 )
 )
 
-(define (_mm256_mask_mov_epi16  src k a %vectsize %lanesize %elemsize %laneoffset )
+(define (_mm256_mask_mov_epi8  src k a %vectsize %lanesize %elemsize %laneoffset %arg0 )
 (apply
 concat
 (for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
  (apply
  concat
  (for/list ([j0.new (reverse (range %laneoffset %lanesize %elemsize))])
-  (define j0.new.div (/  j0.new  %elemsize))
+  (define j0.new.div (/  j0.new  %arg0))
   (define %1 (extract  j0.new.div j0.new.div k))
   (if (equal? %1 (bv #b1 1))
    (begin
@@ -55,14 +55,14 @@ concat
 )
 )
 
-(define (_mm256_mask_abs_epi8  src k a %vectsize %lanesize %elemsize %laneoffset )
+(define (_mm256_mask_abs_epi8  src k a %vectsize %lanesize %elemsize %laneoffset %arg0 )
 (apply
 concat
 (for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
  (apply
  concat
  (for/list ([j0.new (reverse (range %laneoffset %lanesize %elemsize))])
-  (define j0.new.div (/  j0.new  %elemsize))
+  (define j0.new.div (/  j0.new  %arg0))
   (define %1 (extract  j0.new.div j0.new.div k))
   (if (equal? %1 (bv #b1 1))
    (begin
@@ -85,14 +85,14 @@ concat
 )
 )
 
-(define (_mm256_maskz_abs_epi8  k a %vectsize %lanesize %elemsize %laneoffset %arg0 )
+(define (_mm256_maskz_abs_epi8  k a %vectsize %lanesize %elemsize %laneoffset %arg0 %arg1 )
 (apply
 concat
 (for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
  (apply
  concat
  (for/list ([j0.new (reverse (range %laneoffset %lanesize %elemsize))])
-  (define j0.new.div (/  j0.new  %elemsize))
+  (define j0.new.div (/  j0.new  %arg0))
   (define %1 (extract  j0.new.div j0.new.div k))
   (if (equal? %1 (bv #b1 1))
    (begin
@@ -105,7 +105,7 @@ concat
    (begin
    (define %lastidx1 (-  %elemsize  1))
    (define %7 (+  j0.new  %lastidx1))
-   %arg0
+   %arg1
    )
   )
  )
@@ -114,7 +114,7 @@ concat
 )
 )
 
-(define (_mm256_blendv_epi8  a b mask %vectsize %lanesize %elemsize %laneoffset %arg0 )
+(define (_mm_blendv_epi8  a b mask %vectsize %lanesize %elemsize %laneoffset %arg0 )
 (apply
 concat
 (for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
@@ -143,14 +143,14 @@ concat
 )
 )
 
-(define (_mm256_blend_epi16  a b imm8 %vectsize %lanesize %elemsize %laneoffset %arg0 %arg1 )
+(define (_mm256_blend_epi16  a b imm8 %vectsize %lanesize %elemsize %laneoffset %arg0 )
 (apply
 concat
 (for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
  (apply
  concat
  (for/list ([j0.new (reverse (range %laneoffset %lanesize %elemsize))])
-  (define j0.new.div (/  j0.new  %arg1))
+  (define j0.new.div (/  j0.new  %elemsize))
   (define %1 (modulo  j0.new.div  %arg0))
   (define %2 (extract  %1 %1 imm8))
   (if (equal? %2 (bv #b1 1))
@@ -165,35 +165,6 @@ concat
    (define %8 (+  j0.new  %lastidx1))
    (define %9 (extract  %8 j0.new a))
    %9
-   )
-  )
- )
- )
-)
-)
-)
-
-(define (_mm_blend_epi16  a b imm8 %vectsize %lanesize %elemsize %laneoffset )
-(apply
-concat
-(for/list ([%outer.it (reverse (range 0 %vectsize %lanesize))])
- (apply
- concat
- (for/list ([j0.new (reverse (range %laneoffset %lanesize %elemsize))])
-  (define j0.new.div (/  j0.new  %elemsize))
-  (define %1 (extract  j0.new.div j0.new.div imm8))
-  (if (equal? %1 (bv #b1 1))
-   (begin
-   (define %lastidx0 (-  %elemsize  1))
-   (define %3 (+  j0.new  %lastidx0))
-   (define %4 (extract  %3 j0.new b))
-   %4
-   )
-   (begin
-   (define %lastidx1 (-  %elemsize  1))
-   (define %7 (+  j0.new  %lastidx1))
-   (define %8 (extract  %7 j0.new a))
-   %8
    )
   )
  )
