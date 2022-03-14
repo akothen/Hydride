6390616d182ab4717fc63a6ec2991a6c42601ad0
Akash Kothari <akashk4@illinois.edu>
2022-03-13 23:08:27 -0500

    Extend the definition of EquivalentClass class

--- compiler-generator/tools/similarity-checker/RoseEquivalenceClass.py
+++ compiler-generator/tools/similarity-checker/RoseEquivalenceClass.py
@@ -9,12 +9,27 @@
 from RoseAbstractions import *
 from RoseValues import *
 
+import uuid
+
 
 class RoseEquivalenceClass:
   def __init__(self):
     self.EquivalentFuctions = set()
     self.FunctToArgsMapping = dict()
-  
+    # Unique ID for each equivalent class
+    self.ID = uuid.uuid4()
+
+  def __eq__(self, Other):
+    assert isinstance(Other, RoseEquivalenceClass)
+    return self.ID == Other.ID
+
+  def __neq__(self, Other):
+    assert isinstance(Other, RoseEquivalenceClass)
+    return self.ID != Other.ID
+
+  def __hash__(self):
+    return hash(self.ID())
+
   def addFunction(self, Function : RoseFunction):
     self.EquivalentFuctions.add(Function)
   
@@ -24,9 +39,14 @@ class RoseEquivalenceClass:
   def getAFunction(self):
     return self.EquivalentFuctions[0]
   
+  def getFunctToArgsMapping(self):
+    return self.FunctToArgsMapping
+
   def getEquivalentFunctions(self):
     return self.EquivalentFuctions
 
-
+  def extend(self, EquivalentFuctions : set,  FunctToArgsMapping : dict):
+    self.EquivalentFuctions.update(EquivalentFuctions)
+    self.FunctToArgsMapping.update(FunctToArgsMapping)
 
 
