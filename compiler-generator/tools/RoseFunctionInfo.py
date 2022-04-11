21170dc175046364919c6959ebf0908cc87b2ebf
Akash Kothari <akashk4@illinois.edu>
2022-04-10 21:40:07 -0500

    Track more info about semantics in function info

--- compiler-generator/tools/RoseFunctionInfo.py
+++ compiler-generator/tools/RoseFunctionInfo.py
@@ -10,6 +10,8 @@
 from RoseAbstractions import *
 from RoseValues import *
 from RoseContext import *
+from RoseBitVectorOperations import *
+from RoseUtilities import *
 from RoseCodeGenerator import RoseCodeGenerator
 
 
@@ -20,6 +22,12 @@ class RoseFunctionInfo():
     self.InstSema = None
     self.Context = None
     self.CodeGenerator = None
+    self.InElemType = None
+    self.OutElemType = None
+    self.InVectorLength = None
+    self.OutVectorLength = None
+    self.LaneSize = None
+    self.SIMD = None
   
   def addArgsToConcreteMap(self, ArgsToConcreteValMap : dict):
     # Sanity checking
@@ -33,6 +41,9 @@ class RoseFunctionInfo():
 
   def argHasConcreteVal(self, Arg : RoseFunction):
     return Arg in self.ArgsToConcreteValMap
+
+  def getArgsToConcreteValMap(self):
+    return self.ArgsToConcreteValMap
   
   def getRosetteCode(self):
     return self.RosetteCode
@@ -70,5 +81,69 @@ class RoseFunctionInfo():
   
   def getCodeGenerator(self):
     return self.CodeGenerator
+  
+  def computeSemanticsInfo(self):
+    # Function is expected to be canonicalized
+    Function = self.getLatestFunction()
+    # Go over all the blocks in the function
+    BlockList = Function.getRegionsOfType(RoseBlock)
+    for Block in BlockList:
+      for Op in reversed(Block.getOperations()):
+        if isinstance(Op, RoseBVInsertSliceOp):
+          if Op.getInputBitVector() == Function.getReturnValue():
+            if not isinstance(Op.getInsertValue(), RoseConstant):
+              Loop = Block.getParentOfType(RoseForLoop)
+              if not isinstance(Loop, RoseUndefRegion):
+                OuterLoop = Loop.getParentOfType(RoseForLoop)
+                if not isinstance(OuterLoop, RoseUndefRegion):
+                  self.OutElemType = Op.getOutputBitwidth()
+                  LoopStep = OuterLoop.getStep()
+                  assert isinstance(LoopStep, RoseConstant)
+                  self.LaneSize = LoopStep.getValue()
+                  # If block is a reduction block then the output vector
+                  # length is different.
+                  LoopEnd = OuterLoop.getEndIndex()
+                  assert isinstance(LoopEnd, RoseConstant)
+                  if HasReductionPattern(Block):
+                    self.InVectorLength = Op.getOutputBitwidth() \
+                                  * int(LoopEnd.getValue() / LoopStep.getValue())
+                    self.SIMD = False
+                  else:
+                    self.InVectorLength = LoopEnd.getValue()
+                    self.SIMD = True
+          continue
+        if isinstance(Op, RoseBVExtractSliceOp):
+          if Op.getInputBitVector() != Function.getReturnValue():
+            if Op.getOutputBitwidth() != 1:
+              self.InElemType = Op.getOutputBitwidth()
+              Loop = Block.getParentOfType(RoseForLoop)
+              if not isinstance(Loop, RoseUndefRegion):
+                OuterLoop = Loop.getParentOfType(RoseForLoop)
+                if not isinstance(OuterLoop, RoseUndefRegion):
+                  LoopEnd = OuterLoop.getEndIndex()
+                  assert isinstance(LoopEnd, RoseConstant)
+                  self.OutVectorLength = LoopEnd.getValue()
+          continue
+    return
+
+  def print(self):
+    print("******* Printing Function Info ********")
+    print("Function:")
+    self.getLatestFunction().print()
+    print("Semantics Info:")
+    print(self.InstSema)
+    print("InElemType:")
+    print(self.InElemType)
+    print("OutElemType:")
+    print(self.OutElemType)
+    print("InVectorLength:")
+    print(self.InVectorLength)
+    print("OutVectorLength:")
+    print(self.OutVectorLength)
+    print("LaneSize:")
+    print(self.LaneSize)
+    print("SIMD?:")
+    print(self.SIMD)
+    print("***************************************")
 
 
