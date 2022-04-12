958fad21a7a5caf162c1d2277f8e8cddba3c4320
Akash Kothari <akashk4@illinois.edu>
2022-04-12 13:50:13 -0500

    Add support for headers for autogenerated files

--- compiler-generator/tools/RoseToolsUtils.py
+++ compiler-generator/tools/RoseToolsUtils.py
@@ -5,23 +5,22 @@
 ##############################################################
 
 
-import subprocess 
+import subprocess
 
 
 def RunCommand( Cmd : str):
   print(Cmd)
   try:
-    sproc = subprocess.Popen(Cmd, shell=True, stdin=subprocess.PIPE, \
+    SubProcess = subprocess.Popen(Cmd, shell=True, stdin=subprocess.PIPE, \
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout_data, stderr_data = sproc.communicate(timeout=60)
-    stdout_data = stdout_data.decode("utf-8", "ignore")
-    stderr_data = stderr_data.decode("utf-8", "ignore")
-    return stdout_data, stderr_data
+    Stdout, Stderr = SubProcess.communicate(timeout=10)
+    Stdout = Stdout.decode("utf-8", "ignore")
+    Stderr = Stderr.decode("utf-8", "ignore")
+    return Stdout, Stderr
   except OSError:
     return "", "OS Error running {}".format(Cmd)
   except subprocess.TimeoutExpired:
     return "", "Timout Error running {}".format(Cmd)
-
 
 
 def SizeInBytes(Size):
@@ -32,4 +31,17 @@ def SizeInBytes(Size):
   return Numbytes
 
 
+# Header for automatically generated files
+def GenHeadersForAutoGenFiles(CommentSyntax : str):
+  Header = CommentSyntax + "============================== Hydride File =================================\n" \
+         + CommentSyntax + "\n" \
+         + CommentSyntax + " Part of the Hydride Compiler Infrastructure. \n" \
+         + CommentSyntax + " <Placeholder for license information> \n" \
+         + CommentSyntax + "\n" \
+         + CommentSyntax + "=============================================================================\n" \
+         + CommentSyntax + "\n" \
+         + CommentSyntax + " Do NOT modify this file. It is automatically generated. \n" \
+         + CommentSyntax + "\n" \
+         + CommentSyntax + "=============================================================================\n\n\n"
+  return Header
 
