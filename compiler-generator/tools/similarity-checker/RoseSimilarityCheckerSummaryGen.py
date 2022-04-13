83039c82f04777eb67db15d1b9f5bba512205761
Akash Kothari <akashk4@illinois.edu>
2022-04-12 22:31:23 -0500

    Fix similarity checker summary generator again

--- compiler-generator/tools/similarity-checker/RoseSimilarityCheckerSummaryGen.py
+++ compiler-generator/tools/similarity-checker/RoseSimilarityCheckerSummaryGen.py
@@ -7,7 +7,6 @@
 ##############################################################
 
 
-from tokenize import String
 from RoseAbstractions import *
 from RoseContext import *
 from RosetteCodeEmitter import *
@@ -25,40 +24,46 @@ class RoseSimilarityCheckerSummaryGen():
     self.FunctionToRosetteCodeMap = FunctionToRosetteCodeMap
     self.EquivalenceClasses = EquivalenceClasses
 
+  def genDictionarySubEntry(self, Function : RoseFunction):
+    print("FUNCTION IN EQUIVALENCE CLASS:")
+    Function.print()
+    FunctionInfo = self.FunctionToFunctionInfo[Function]
+    ArgList = list()
+    for Arg in Function.getArgs():
+      if FunctionInfo.argHasConcreteVal(Arg) == False:
+        ArgList.append("\"SYMBOLIC_BV_" + str(Arg.getType().getBitwidth()) + "\"")
+      else:
+        ConcreteVal = FunctionInfo.getConcreteValFor(Arg)
+        ArgList.append("\"" + GenConcreteValue(ConcreteVal) + "\"")
+    TempEntryString = f'''
+                  "args" : {"[" + ",".join(ArgList) + "]"},
+                  "in_vectsize" : {str(FunctionInfo.getInVectorLength())},
+                  "out_vectsize" : {str(FunctionInfo.getOutVectorLength())},
+                  "lanesize" : {str(FunctionInfo.getLaneSize())},
+                  "in_precision" : {str(FunctionInfo.getInElemType())},
+                  "out_precision" : {str(FunctionInfo.getOutElemType())},
+                  "in_vectsize_index" : {str(FunctionInfo.getInVectorLengthIndex())},
+                  "out_vectsize_index" : {str(FunctionInfo.getOutVectorLengthIndex())},
+                  "lanesize_index" : {str(FunctionInfo.getLaneSizeIndex())},
+                  "in_precision_index" : {str(FunctionInfo.getInElemTypeIndex())},
+                  "out_precision_index" : {str(FunctionInfo.getOutElemTypeIndex())},
+                  "Cost" : "{""}",
+                  "SIMD" : "{str(FunctionInfo.isSIMD())}",
+      '''
+    TempEntryString = "{" + TempEntryString + "}"
+    return TempEntryString
+
   def genDictionaryEntry(self, EquivalenceClass : RoseEquivalenceClass):
     FunctionName = EquivalenceClass.getAFunction().getName()
     # List of arguments for functions in this equivalent class
     # and  track names of functions and track vector information
     EntryString = ""
     for Function in EquivalenceClass.getEquivalentFunctions():
-      print("FUNCTION IN EQUIVALENCE CLASS:")
-      Function.print()
-      FunctionInfo = self.FunctionToFunctionInfo[Function]
-      ArgList = list()
-      for Arg in Function.getArgs():
-        if FunctionInfo.argHasConcreteVal(Arg) == False:
-          ArgList.append("\"SYMBOLIC_BV_" + str(Arg.getType().getBitwidth()) + "\"")
-        else:
-          ConcreteVal = FunctionInfo.getConcreteValFor(Arg)
-          ArgList.append("\"" + GenConcreteValue(ConcreteVal) + "\"")
       TempEntryString = f'''
-            "name" : "{Function.getName()}",
-            "args" : {"[" + ",".join(ArgList) + "]"},
-            "in_vectsize" : {str(FunctionInfo.getInVectorLength())},
-            "out_vectsize" : {str(FunctionInfo.getOutVectorLength())},
-            "lanesize" : {str(FunctionInfo.getLaneSize())},
-            "in_precision" : {str(FunctionInfo.getInElemType())},
-            "out_precision" : {str(FunctionInfo.getOutElemType())},
-            "in_vectsize_index" : {str(FunctionInfo.getInVectorLengthIndex())},
-            "out_vectsize_index" : {str(FunctionInfo.getOutVectorLengthIndex())},
-            "lanesize_index" : {str(FunctionInfo.getLaneSizeIndex())},
-            "in_precision_index" : {str(FunctionInfo.getInElemTypeIndex())},
-            "out_precision_index" : {str(FunctionInfo.getOutElemTypeIndex())},
-            "Cost" : "{""}",
-            "SIMD" : "{str(FunctionInfo.isSIMD())}",
+              "{Function.getName()}" : {self.genDictionarySubEntry(Function)},
         '''
-      TempEntryString = "{\n" + TempEntryString + "}"
       EntryString += TempEntryString
+    EntryString = "{" + EntryString + "}"
     # Get the rosette code for this equivalence class
     RosetteCode = self.FunctionToRosetteCodeMap[EquivalenceClass.getAFunction()]
     # Replace all newlines with quotes
@@ -66,10 +71,10 @@ class RoseSimilarityCheckerSummaryGen():
     for Index, Line in enumerate(RosetteCodeList):
       RosetteCodeList[Index] = "\"" + Line + "\""
     String = f'''
-          "x86_instructions" : {EntryString},
-          "semantics" : {RosetteCodeList}, 
+            "x86_instructions" : {EntryString},
+            "semantics" : {RosetteCodeList}, 
       '''
-    String = "{\n" + String + "}"
+    String = "{" + String + "}"
     return String
   
   def genDictionary(self):
@@ -79,7 +84,7 @@ class RoseSimilarityCheckerSummaryGen():
       TempEntryString = f'''
           "{FunctionName}" : {self.genDictionaryEntry(EquivalenceClass)},
         '''
-      String += TempEntryString + "\n"
+      String += TempEntryString
     String += "}\n"
     return String
 
