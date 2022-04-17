e251c28014f95184cc953dc658c72ce1b6c6e36e
Akash Kothari <akashk4@illinois.edu>
2022-04-16 23:36:16 -0500

    Fix function inliner pass

--- compiler-generator/transform/RoseFunctionInliner.py
+++ compiler-generator/transform/RoseFunctionInliner.py
@@ -1,3 +1,9 @@
+#############################################################
+#
+# This is the function inlining pass for Rose IR.
+#
+#############################################################
+
 
 from RoseType import RoseType
 from RoseValue import RoseValue
@@ -6,9 +12,10 @@ from RoseValues import *
 from RoseBitVectorOperation import *
 from RoseBitVectorOperations import *
 from RoseOperations import *
+from RoseContext import *
 
 
-def RunInlinerOnFunction(Function : RoseFunction):
+def RunInlinerOnFunction(Function : RoseFunction, Context : RoseContext):
   print("RUN INLINER ON FUNCTION")
   print("FUNCTION:")
   Function.print()
@@ -17,22 +24,30 @@ def RunInlinerOnFunction(Function : RoseFunction):
   for Abstraction in Function.getChildren():
     # Run inliner on the nested function
     if isinstance(Abstraction, RoseFunction):
-      RunInlinerOnFunction(Abstraction)
+      RunInlinerOnFunction(Abstraction, Context)
   
   # Top level function has no uses
   if Function.isTopLevelFunction():
     return
   
-  # Get the params of the function
-  FunctionArgs = Function.getArgs()
+  # Track call sites of the function
+  CallSites = list()
 
   # Look for uses of this function
   ParentRegionOfFunction = Function.getFunction()
   Users = ParentRegionOfFunction.getUsersOf(Function)
-  for User in Users:
+  Suffix = "callsite"
+  for UserIndex, User in enumerate(Users):
+    # Clone the function
+    ClonedFunction = Function.clone(Suffix + str(UserIndex))
+    print("ClonedFunction:")
+    ClonedFunction.print()
+    # Get the params of the function
+    FunctionArgs = ClonedFunction.getArgs()
     print("USER OF FUNCTION:")
     User.print()
     assert isinstance(User, RoseCallOp)
+    CallSites.append(User)
     # Get all the call operands that matter
     CallArgs = User.getCallOperands()
     Block = User.getParent()
@@ -48,11 +63,32 @@ def RunInlinerOnFunction(Function : RoseFunction):
     # and add them to the parent region.
     ParentRegion = Block.getParent()
     InsertionPoint = ParentRegion.getPosOfChild(NewBlock)
-    for Region in Function.getChildren():
-      ParentRegion.addRegionBefore(InsertionPoint, Region.clone())
+    for Region in ClonedFunction.getChildren():
+      ParentRegion.addRegionBefore(InsertionPoint, Region)
+      InsertionPoint += 1
+    print("ParentRegion:")
+    ParentRegion.print()
     # Now replace the uses of function args with call args
-    for Index in range(Function.getNumArgs()):
-      ParentRegion.replaceUsesWith(FunctionArgs, CallArgs[Index])
+    for Index in range(ClonedFunction.getNumArgs()):
+      print("CallArgs[Index]:")
+      CallArgs[Index].print()
+      print("FunctionArgs[Index]:")
+      FunctionArgs[Index].print()
+      ParentRegion.replaceUsesWith(FunctionArgs[Index], CallArgs[Index])
+    # Replace the value at callsite with return value
+    User.replaceUsesWith(ClonedFunction.getReturnValue())
+    # Erase the return op
+    for Op in ParentRegion.getUsersOf(ClonedFunction.getReturnValue()):
+      if isinstance(Op, RoseReturnOp):
+        ParentBlock = Op.getParent()
+        ParentBlock.eraseOperation(Op)
+        if ParentBlock.getNumOperations() == 0:
+          BlockParent = ParentBlock.getParent()
+          Key = BlockParent.getKeyForChild(ParentBlock)
+          BlockParent.eraseChild(ParentBlock, Key)
+        break
+    print("ParentRegion:")
+    ParentRegion.print()
     print("FUNCTION AFTER INLINING")
     ParentRegionOfFunction.print()
   
@@ -62,13 +98,16 @@ def RunInlinerOnFunction(Function : RoseFunction):
   ParentRegionOfFunction.eraseChild(Function)
   print("FUNCTION AFTER ERASING")
   ParentRegionOfFunction.print()
+  # Erase call sites
+  for CallSite in CallSites:
+    Block = CallSite.getParent()
+    Block.eraseOperation(CallSite)
 
 
-
-# Runs Loop reroller
-def Run(Function : RoseFunction):
+# Runs function inliner
+def Run(Function : RoseFunction, Context : RoseContext):
   print("RUN FUNCTION INLINER")
-  RunInlinerOnFunction(Function)
+  RunInlinerOnFunction(Function, Context)
   Function.print()
 
 
