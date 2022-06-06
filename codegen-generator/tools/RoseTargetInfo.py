889f56c294114baa34e74da05ddafc0a8f6587c4
Akash Kothari <akashk4@illinois.edu>
2022-06-05 23:13:53 -0500

    Add class for targetinfo

---
+++ codegen-generator/tools/RoseTargetInfo.py
@@ -0,0 +1,22 @@
+#############################################################
+#
+# This is the class that contains target information.
+#
+#############################################################
+
+
+import uuid
+
+
+class RoseTargetInfo:
+  def __init__(self, TargetName : str):
+    self.TargetName = TargetName
+    self.ID = uuid.uuid4()
+  
+  def getTargetName(self):
+    return self.TargetName
+  
+  def __hash__(self):
+    return hash(self.ID)
+
+
