369a631aede01bcd71d784ad058840dcc3533da9
RafaeNoor <abdurrafae98@live.com>
2022-07-19 14:33:53 -0500

    Adding synthesized references

---
+++ code-synthesizer/expr-references/sobel.rkt
@@ -0,0 +1,114 @@
+(_mm512_min_epu64_dsl
+ (_m_paddw_dsl
+  (_mm512_sub_epi8_dsl
+   (_m_pmaxub_dsl
+    (_m_paddw_dsl
+     (_m_paddw_dsl (reg 9) (reg 8) 512 512 512 16 0)
+     (reg 7)
+     512
+     512
+     512
+     16
+     0)
+    (_m_paddw_dsl
+     (reg 5)
+     (_m_paddw_dsl (reg 2) (reg 6) 512 512 512 16 0)
+     512
+     512
+     512
+     16
+     0)
+    512
+    512
+    512
+    16
+    0)
+   (_mm512_min_epu64_dsl
+    (_m_paddw_dsl
+     (_m_paddw_dsl (reg 9) (reg 8) 512 512 512 16 0)
+     (reg 7)
+     512
+     512
+     512
+     16
+     0)
+    (_m_paddw_dsl
+     (reg 5)
+     (_m_paddw_dsl (reg 2) (reg 6) 512 512 512 16 0)
+     512
+     512
+     512
+     16
+     0)
+    512
+    512
+    512
+    16
+    0)
+   512
+   512
+   512
+   16
+   0)
+  (_mm512_sub_epi8_dsl
+   (_m_pmaxub_dsl
+    (_m_paddw_dsl
+     (_m_paddw_dsl (reg 0) (reg 1) 512 512 512 16 0)
+     (reg 2)
+     512
+     512
+     512
+     16
+     0)
+    (_m_paddw_dsl
+     (_m_paddw_dsl (reg 3) (reg 4) 512 512 512 16 0)
+     (reg 5)
+     512
+     512
+     512
+     16
+     0)
+    512
+    512
+    512
+    16
+    0)
+   (_mm512_min_epu64_dsl
+    (_m_paddw_dsl
+     (_m_paddw_dsl (reg 3) (reg 4) 512 512 512 16 0)
+     (reg 5)
+     512
+     512
+     512
+     16
+     0)
+    (_m_paddw_dsl
+     (_m_paddw_dsl (reg 0) (reg 1) 512 512 512 16 0)
+     (reg 2)
+     512
+     512
+     512
+     16
+     0)
+    512
+    512
+    512
+    16
+    0)
+   512
+   512
+   512
+   16
+   0)
+  512
+  512
+  512
+  16
+  0)
+ (lit
+  (bv #x00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff 512))
+ 512
+ 512
+ 512
+ 16
+ 0)
