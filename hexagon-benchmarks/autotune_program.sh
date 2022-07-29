00ad9f19656609a708edf4e41a4ba0434d1899de
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2022-07-28 23:25:56 -0500

    Fixed auto-tuning to work with 2019 infra

--- hexagon-benchmarks/autotune_program.sh
+++ hexagon-benchmarks/autotune_program.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 ################### CHANGE THESE ###################
-HALIDE_ROOT=/home/sb54/halide-14
+HALIDE_ROOT=/home/sb54/Hydride/hexagon-benchmarks/halide_standalone_autoscheduler
 GEN_NAME=median3x3
 ####################################################
 
-HALIDE_DISTRIB=$HALIDE_ROOT/distrib
-HALIDE_LIB=$HALIDE_DISTRIB/lib
-ADAMS_ROOT=$HALIDE_ROOT/src/autoschedulers/adams2019
-
 # First, use run.sh to create the generator. It should create a generator in the current dir.
-./run.sh $GEN_NAME > /dev/null 2>&1
+# ./run.sh $GEN_NAME > /dev/null 2>&1
 GEN_EXE=/home/sb54/Hydride/hexagon-benchmarks/gen
 
+HALIDE_INCLUDE=$HALIDE_ROOT/distrib/include
+HALIDE_TOOLS=$HALIDE_ROOT/distrib/tools
+HALIDE_LIB=$HALIDE_ROOT/distrib/lib
+HALIDE_AUTOSCHED=$HALIDE_ROOT/apps/autoscheduler
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HALIDE_LIB
-$ADAMS_ROOT/autotune_loop.sh $GEN_EXE $GEN_NAME host $ADAMS_ROOT/baseline.weights $HALIDE_ROOT/bin $HALIDE_DISTRIB samples_out
+SAVE_CURR_DIR=$PWD
+echo $SAVE_CURR_DIR
+
+# autotune_loop.sh uses relative paths so we need to do this
+cd $HALIDE_AUTOSCHED
+# This will create a directory `samples` in the current path. Delete it if you want to start over
+# the auto-tuning, otherwise these weights are iteratively improved.
+./autotune_loop.sh $GEN_EXE	$GEN_NAME	host ./weights ./bin
