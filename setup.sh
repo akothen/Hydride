853b6abf163916a040f3cc3ff10d0ee6e5711d22
Akash Kothari <akashk4@illinois.edu>
2022-08-16 10:04:43 -0500

    Add a way to set up paths to run Hydride

---
+++ setup.sh
@@ -0,0 +1,3 @@
+CUR_DIR=$(pwd)
+
+export PYTHONPATH=$CUR_DIR/codegen-generator/targets/x86/:$CUR_DIR/codegen-generator/codegen/rosette/:$CUR_DIR/codegen-generator/codegen/llvm/:$CUR_DIR/codegen-generator/rosette-ir/:$CUR_DIR/codegen-generator/tools/code-generator/:$CUR_DIR/codegen-generator/tools/fuzzer:$CUR_DIR/codegen-generator/tools/rosette_lifter/:$CUR_DIR/codegen-generator/tools/similarity-checker:$CUR_DIR/codegen-generator/transform
