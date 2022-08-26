567269f048231ace9f02a4076a27f0a8673a8dd1
Akash Kothari <akashk4@illinois.edu>
2022-08-26 02:52:34 -0500

    Fix Rosette parser implementation

--- codegen-generator/tools/rosette_lifter/RosetteParser.py
+++ codegen-generator/tools/rosette_lifter/RosetteParser.py
@@ -12,56 +12,50 @@ class RosetteParser:
   # Tokenizer for Rosette
   class RosetteTokenizer:
     Tokenizer = r'''\s*(,@|[('`,)]|"(?:[\\].|[^\\"])*"|;.*|[^\s('"`,;)]*)(.*)'''
-    EOF = "EOF"
     
-    def __init__(self, File):
-      self.File = File
-      self.Line = ''
+    def __init__(self, RosetteCode):
+      self.RosetteCode = RosetteCode
 
     def nextToken(self):
-      while True:
-        if self.Line == '': 
-          self.Line = self.File.readline()
-        if self.Line == '': 
-          return RosetteParser.RosetteTokenizer.EOF
-        Token, self.Line = re.match(RosetteParser.RosetteTokenizer.Tokenizer, \
-                                    self.Line).groups()
+      for Line in self.RosetteCode:
+        Token, Line = re.match(RosetteParser.RosetteTokenizer.Tokenizer, Line).groups()
         if Token != '' and not Token.startswith('; '):
             return Token
+      return None
   
   def tokenize(self, File):
-      Lexer = self.RosetteTokenizer(File)
-      Token = Lexer.nextToken()
-      Tokens = []
-      while Token != "EOF":
-          Tokens.append(Token)
-          Token = Lexer.nextToken()
-      return Tokens
+    Lexer = self.RosetteTokenizer(File)
+    Token = Lexer.nextToken()
+    Tokens = list()
+    while Token != None:
+        Tokens.append(Token)
+        Token = Lexer.nextToken()
+    return Tokens
 
   def atom(self, Token: str):
-      "Numbers become numbers; every other token is a symbol."
-      try: return int(Token)
-      except ValueError:
-          try: return float(Token)
-          except ValueError:
-              return str(Token)
+    # Numbers become numbers; every other token is a symbol.
+    try: return int(Token)
+    except ValueError:
+        try: return float(Token)
+        except ValueError:
+            return str(Token)
 
   def readExpression(self, Tokens : list):
-      "Read an expression from a sequence of tokens."
-      if len(Tokens) == 0:
-          raise SyntaxError('unexpected EOF')
-      token = Tokens.pop(0)
-      if token == '(':
-          L = []
-          while Tokens[0] != ')':
-              L.append(self.read_from_tokens(Tokens))
-          Tokens.pop(0) # pop off ')'
-          return L
-      elif token == ')':
-          raise SyntaxError('unexpected )')
-      else:
-          return self.atom(token)
+    "Read an expression from a sequence of tokens."
+    if len(Tokens) == 0:
+      raise SyntaxError('NO TOKENS IN ROSETTE CODE')
+    Token = Tokens.pop(0)
+    if Token == '(':
+      L = []
+      while Tokens[0] != ')':
+          L.append(self.readExpression(Tokens))
+      Tokens.pop(0) # pop off ')'
+      return L
+    elif Token == ')':
+        raise SyntaxError('unexpected )')
+    else:
+        return self.atom(Token)
 
   @staticmethod
   def parse(self, File):
-      return self.readExpression(self.tokenize(File))
+    return self.readExpression(self.tokenize(File))
