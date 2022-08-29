d95783453978276a6c65a1985a89b15aa3ba7dd0
Akash Kothari <akashk4@illinois.edu>
2022-08-28 22:00:52 -0500

    Make the parser functional

--- codegen-generator/tools/rosette_lifter/RosetteParser.py
+++ codegen-generator/tools/rosette_lifter/RosetteParser.py
@@ -15,47 +15,74 @@ class RosetteParser:
     
     def __init__(self, RosetteCode):
       self.RosetteCode = RosetteCode
+      self.Line = ""
+      self.Index = 0
+      print("self.RosetteCode")
+      print( self.RosetteCode)
 
     def nextToken(self):
-      for Line in self.RosetteCode:
-        Token, Line = re.match(RosetteParser.RosetteTokenizer.Tokenizer, Line).groups()
-        if Token != '' and not Token.startswith('; '):
-            return Token
+      while True:
+        if self.Line == "":
+          if self.Index != len(self.RosetteCode) - 1:
+            self.Line = self.RosetteCode[self.Index]
+            self.Index += 1
+          else:
+            break
+        print("PARSED LINE:")
+        print(self.Line)
+        Token, self.Line = re.match(RosetteParser.RosetteTokenizer.Tokenizer, \
+                                    self.Line.strip()).groups()
+        print("TOKEN:")
+        print(Token)
+        print("Line:")
+        print(self.Line)
+        if Token != '':
+          print("--")
+          return Token
       return None
   
-  def tokenize(self, File):
-    Lexer = self.RosetteTokenizer(File)
+  def tokenize(self, Code):
+    Lexer = self.RosetteTokenizer(Code)
     Token = Lexer.nextToken()
     Tokens = list()
     while Token != None:
         Tokens.append(Token)
         Token = Lexer.nextToken()
+    print("LIST OF TOKENS:")
+    print(Tokens)
     return Tokens
 
-  def atom(self, Token: str):
+  def transformType(self, Token: str):
     # Numbers become numbers; every other token is a symbol.
-    try: return int(Token)
+    try: 
+      return int(Token)
     except ValueError:
-        try: return float(Token)
+        try: 
+          return float(Token)
         except ValueError:
-            return str(Token)
+          return str(Token)
 
   def readExpression(self, Tokens : list):
-    "Read an expression from a sequence of tokens."
+    # Read an expression from a sequence of tokens.
     if len(Tokens) == 0:
       raise SyntaxError('NO TOKENS IN ROSETTE CODE')
     Token = Tokens.pop(0)
     if Token == '(':
-      L = []
+      ExprLst = list()
       while Tokens[0] != ')':
-          L.append(self.readExpression(Tokens))
-      Tokens.pop(0) # pop off ')'
-      return L
+        ExprLst.append(self.readExpression(Tokens))
+      Tokens.pop(0)
+      return ExprLst
     elif Token == ')':
         raise SyntaxError('unexpected )')
     else:
-        return self.atom(Token)
+        return self.transformType(Token)
 
   @staticmethod
-  def parse(self, File):
-    return self.readExpression(self.tokenize(File))
+  def parse(Code):
+    Parser = RosetteParser()
+    ParsedExpr = Parser.readExpression(Parser.tokenize(Code))
+    print("ParsedExpr:")
+    print(ParsedExpr)
+    return ParsedExpr
+
