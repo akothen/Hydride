7f81e253017aa37f8a9fd9779d5cc1831279a1bf
Akash Kothari <akashk4@illinois.edu>
2022-08-30 13:58:41 -0500

    Add RoseValue to LLVM Type information

--- codegen-generator/codegen/llvm/RoseLLVMContext.py
+++ codegen-generator/codegen/llvm/RoseLLVMContext.py
@@ -8,12 +8,16 @@
 from RoseAbstractions import *
 from RoseValues import *
 
-from llvmlite import ir
+from llvmlite.ir.values import Value as LLVMValue
+from llvmlite.ir.builder import IRBuilder as LLVMIRBuilder
+from llvmlite.ir.context import Context as LLVMContext
+from llvmlite.ir.types import Type as LLVMType
 
 
-class RoseLLVMContext(ir.context.Context):
+class RoseLLVMContext(LLVMContext):
   def __init__(self):
     self.RoseToLLVMVal = dict()
+    self.RoseToLLVMType = dict()
     self.LLVMBuilder = None
     super().__init__()
   
@@ -21,7 +25,7 @@ class RoseLLVMContext(ir.context.Context):
   def create():
     return RoseLLVMContext()
   
-  def mapRoseValToLLVMVal(self, RoseVal : RoseValue, LLVMVal : ir.Value):
+  def mapRoseValToLLVMVal(self, RoseVal : RoseValue, LLVMVal : LLVMValue):
     self.RoseToLLVMVal[RoseVal] = LLVMVal
   
   def getLLVMValueFor(self, RoseVal : RoseValue):
@@ -29,9 +33,18 @@ class RoseLLVMContext(ir.context.Context):
       return self.RoseToLLVMVal[RoseVal]
     return ir.Undefined()
   
-  def setLLVMBuilder(self, LLVMBuilder : ir.IRBuilder):
+  def setLLVMBuilder(self, LLVMBuilder : LLVMIRBuilder):
     self.LLVMBuilder = LLVMBuilder
   
   def getLLVMBuilder(self):
     return self.LLVMBuilder
+
+  def getLLVMTypeFor(self, RoseVal : RoseValue):
+    if RoseVal in self.RoseToLLVMType:
+      return self.RoseToLLVMType[RoseVal]
+    return None
+  
+  def setLLVMTypeFor(self, RoseVal : RoseValue, Type : LLVMType):
+    assert isinstance(Type, LLVMType)
+    self.RoseToLLVMType[RoseVal] = Type
 
