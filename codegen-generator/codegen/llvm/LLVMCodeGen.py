04cbaafd80a76d1fd486ea1ead188958816f0025
Akash Kothari <akashk4@illinois.edu>
2022-08-31 15:47:34 -0500

    Add a fully functional LLVM generator

--- codegen-generator/codegen/llvm/LLVMCodeGen.py
+++ codegen-generator/codegen/llvm/LLVMCodeGen.py
@@ -5,7 +5,6 @@
 #############################################################
 
 
-from webbrowser import Opera
 from RoseAbstractions import *
 from RoseValues import *
 from RoseOperations import *
@@ -13,78 +12,142 @@ from RoseBitVectorOperations import *
 from RoseUtilities import *
 from RoseLLVMContext import *
 
-from RoseToLLVMMap import *  # This is an auto-generated file.
-
+from llvmlite.ir.module import Module as LLVMModule
 from llvmlite.ir.values import Function as LLVMFunction
-from llvmlite.ir.types import FunctionType as LLVMVFunctionType
+from llvmlite.ir.types import FunctionType as LLVMFunctionType
 from llvmlite.ir.builder import IRBuilder as LLVMIRBuilder
 
 
 
-def CreateFunctionType(RoseIRFunction : RoseFunction, LLVMContext : RoseLLVMContext):
+def CreateFunctionTypeForFunction(RoseIRFunction : RoseFunction, LLVMContext : RoseLLVMContext):
   RetVal = RoseIRFunction.getReturnValue()
   RetType = LLVMContext.getLLVMTypeFor(RetVal)
+  print("RetType:")
+  print(RetType)
   InputArgTypes = list()
   for Arg in RoseIRFunction.getArgs():
+    print("Arg:")
+    Arg.print()
+    print("LLVMContext.getLLVMTypeFor(Arg):")
+    print(LLVMContext.getLLVMTypeFor(Arg))
     InputArgTypes.append(LLVMContext.getLLVMTypeFor(Arg))
-  return LLVMVFunctionType(RetType, InputArgTypes)
+  return LLVMFunctionType(RetType, InputArgTypes)
+
+
+def CreateFunctionTypeForOp(RoseOp : RoseOpaqueCallOp, LLVMContext : RoseLLVMContext):
+  OutType = LLVMContext.getLLVMTypeFor(RoseOp)
+  print("OutType:")
+  print(OutType)
+  OperandTypes = list()
+  for Operand in RoseOp.getCallOperands():
+    print("Operand:")
+    Operand.print()
+    if isinstance(Operand, RoseConstant):
+      if isinstance(Operand.getType(), RoseIntegerType):
+        OperandTypes.append(LLVMIntType(Operand.getType().getBitwidth()))
+      else:
+        assert isinstance(Operand.getType(), RoseBitVectorType)
+
+    else:
+      OperandTypes.append(LLVMContext.getLLVMTypeFor(Operand))
+  return LLVMFunctionType(OutType, OperandTypes)
+
+
+def get_function(builder, fn_name, ret_type, tys):
+    fnty = LLVMFunctionType(ret_type, tys)
+    #suffixes = [t.intrinsic_name for t in tys]
+    fn_name = '.'.join([fn_name] + [])
+    if fn_name in builder.module.globals:
+        fn = builder.module.globals[fn_name]
+    else:
+        fn = LLVMFunction(builder.module, fnty, name=fn_name)
+    return fn
 
 
 def GenerateLLVMBlock(LLVMIRFunction : LLVMFunction, RoseIRBlock : RoseBlock, \
-                      LLVMContext : RoseLLVMContext, RosetteContext : RoseContext):
-  LLVMBlock = LLVMIRFunction.append_basic_block(name=RosetteContext.genName())
-  Builder = LLVMIRBuilder(LLVMBlock)
+                      LLVMContext : RoseLLVMContext):
+  LLVMBlock = LLVMIRFunction.append_basic_block(name="entry")
+  LLVMContext.setLLVMBuilder(LLVMIRBuilder(LLVMBlock))
   for Op in RoseIRBlock:
     # There should be no call operation except opaque calls
     assert not isinstance(Op, RoseCallOp)
     if isinstance(Op, RoseOpaqueCallOp):
       # Get the name of the LLVM intrinsic
-      LLVMIntrinsicName = RoseToLLVMMap[Op.getCallee().getName()]
+      LLVMIntrinsicName = "llvm.hydride." + Op.getCallee().getName()
+      RetType = LLVMContext.getLLVMTypeFor(Op)
+      OperandTypes = list()
       Args = list()
       for Operand in Op.getCallOperands():
+        print("Operand:")
+        Operand.print()
         if isinstance(Operand, RoseConstant):
-          Args.append(Operand.to_llvm_ir())
+          if isinstance(Operand.getType(), RoseIntegerType):
+            OperandTypes.append(LLVMIntType(Operand.getType().getBitwidth()))
+          else:
+            assert isinstance(Operand.getType(), RoseBitVectorType)
+            print("CONSTANT BITVECTOR")
+            print("LLVMContext.getLLVMTypeFor(Operand):")
+            print(LLVMContext.getLLVMTypeFor(Operand))
+            OperandTypes.append(LLVMContext.getLLVMTypeFor(Operand))
+          print("Operand.to_llvm_ir(LLVMContext):")
+          print(Operand.to_llvm_ir(LLVMContext))
+          Args.append(Operand.to_llvm_ir(LLVMContext))
         else:
+          OperandTypes.append(LLVMContext.getLLVMTypeFor(Operand))
           Args.append(LLVMContext.getLLVMValueFor(Operand))
-      CallInst = Builder.call(LLVMIntrinsicName, Args)
+      print("OperandTypes:")
+      print(OperandTypes)
+      Intrinsic = get_function(LLVMContext.getLLVMBuilder(), LLVMIntrinsicName, RetType, OperandTypes)
+      CallInst = LLVMContext.getLLVMBuilder().call(Intrinsic, Args, Op.getName())
+      print("CallInst:")
+      print(CallInst)
       LLVMContext.mapRoseValToLLVMVal(Op, CallInst)
       continue
+    print("OP:")
+    Op.print()
     Instruction = Op.to_llvm_ir(LLVMContext)
     LLVMContext.mapRoseValToLLVMVal(Op, Instruction)
 
 
 def GenerateRegion(LLVMIRFunction : LLVMFunction, Region : RoseRegion,
-                   LLVMContext : RoseLLVMContext, RosetteContext : RoseContext):
+                   LLVMContext : RoseLLVMContext):
   # TODO: Support LLVM codegen for other types of regions
   for SubRegion in Region:
     # LLVM does not support nested functions
     assert not isinstance(SubRegion, RoseFunction)
     if isinstance(SubRegion, RoseBlock):
-      GenerateLLVMBlock(LLVMIRFunction, SubRegion, LLVMContext, RosetteContext)
+      GenerateLLVMBlock(LLVMIRFunction, SubRegion, LLVMContext)
       continue
     assert(False and "Attempting to generate LLVM IR for unsupported Rose Region.")
 
 
-def GenerateLLVMIR(RoseIRFunction : RoseFunction, RosetteContext : RoseContext):
+def GenerateLLVMIR(RoseIRFunction : RoseFunction, RoseLLVMCxt : RoseLLVMContext):
   # Create an empty module and function.
-  LLVMModule = LLVMFunction(name = RoseIRFunction.getName() + ".module")
-  LLVMContext = RoseLLVMContext()
-  LLVMFunctionType = CreateFunctionType(RoseIRFunction, LLVMContext)
-  LLVMIRFunction = LLVMFunction(LLVMModule, LLVMFunctionType, name=RoseIRFunction.getName())
+  LLVMIRModule = LLVMModule(name = RoseIRFunction.getName() + ".module")
+  print("RoseIRFunction:")
+  RoseIRFunction.print()
+  LLVMFunctionType = CreateFunctionTypeForFunction(RoseIRFunction, RoseLLVMCxt)
+  print("LLVMFunctionType:")
+  print(LLVMFunctionType)
+  LLVMIRFunction = LLVMFunction(LLVMIRModule, LLVMFunctionType, name=RoseIRFunction.getName())
   # Map LLVM function arguments to the Rose function arguments
   for Idx, Arg in enumerate(LLVMIRFunction.args):
-    LLVMContext.mapRoseValToLLVMVal(RoseIRFunction.getArg(Idx), Arg)
-  GenerateRegion(LLVMIRFunction, RoseIRFunction, LLVMContext, RosetteContext)
-  return LLVMModule
+    RoseLLVMCxt.mapRoseValToLLVMVal(RoseIRFunction.getArg(Idx), Arg)
+    print(RoseLLVMCxt.getLLVMValueFor(RoseIRFunction.getArg(Idx)))
+    print("RoseIRFunction.getArg(Idx)")
+    print(id(RoseIRFunction.getArg(Idx)))
+  GenerateRegion(LLVMIRFunction, RoseIRFunction, RoseLLVMCxt)
+  return LLVMIRModule
 
 
 # Converts the given Rose IR function into Rosette
-def CodeGen(Function : RoseFunction, RosetteContext : RoseContext):
-  LLVMModule = GenerateLLVMIR(Function, RosetteContext)
+def LLVMCodeGen(Function : RoseFunction, RoseLLVMCxt : RoseLLVMContext):
+  LLVMIRModule = GenerateLLVMIR(Function, RoseLLVMCxt)
   print("---\n\n\n\n\n")
   Function.print()
   print("LLVM Module")
-  print(LLVMModule)
+  print(LLVMIRModule)
   print("---\n\n\n\n\n")
+  return LLVMIRModule
 
 
