35a4eb1e2855299aa694cae914e0835cb6fbbdd0
Akash Kothari <akashk4@illinois.edu>
2022-08-31 23:59:08 -0500

    Make setup shell script more readable and fix some paths

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
+
 CUR_DIR=$(pwd)
 
-export PYTHONPATH=$CUR_DIR/codegen-generator/targets/x86/:$CUR_DIR/codegen-generator/codegen/rosette/:$CUR_DIR/codegen-generator/codegen/llvm/:$CUR_DIR/codegen-generator/rosette-ir/:$CUR_DIR/codegen-generator/tools/:$CUR_DIR/codegen-generator/tools/code-generator/:$CUR_DIR/codegen-generator/tools/fuzzer:$CUR_DIR/codegen-generator/tools/rosette_lifter/:$CUR_DIR/codegen-generator/tools/similarity-checker:$CUR_DIR/codegen-generator/transform
+export PYTHONPATH=$CUR_DIR/codegen-generator/targets/x86/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/codegen/rosette/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/codegen/llvm/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/rosette-ir/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/code-generator/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/fuzzer:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/rosette-lifter/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/low-level-codegen/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/similarity-checker:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/transform:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/llvmlite:$PYTHONPATH
+
+export PATH=/home/akashk4/Hydride/llvm-project/build/bin/:$PATH
+export PATH=/home/akashk4/Hydride/rosette/bin/:$PATH
