e162b8cfc40d6988b69d993f44687269c27d1517
Akash Kothari <akashk4@illinois.edu>
2022-10-07 02:00:31 -0500

    Fix the size of temp registers in bvreduction pass

--- codegen-generator/transform/RoseBVLengthReduction.py
+++ codegen-generator/transform/RoseBVLengthReduction.py
@@ -20,11 +20,7 @@ def RunBVLengthReductionOnFunction(Function : RoseFunction, Context : RoseContex
   assert isinstance(Function, RoseFunction)
   # Check if the return value is coming from the extract op.
   ReturnVal = Function.getReturnValue()
-  print("ReturnVal:")
-  ReturnVal.print()
   if isinstance(ReturnVal, RoseBVExtractSliceOp):
-    print("ReturnVal.getInputBitVector():")
-    ReturnVal.getInputBitVector().print()
     Block = ReturnVal.getParent()
     if ReturnVal.getInputBitVector().getType().getBitwidth() == ReturnVal.getType().getBitwidth():
       # Replace the return value with the input bitvector in the return op
@@ -33,43 +29,89 @@ def RunBVLengthReductionOnFunction(Function : RoseFunction, Context : RoseContex
       Function.setRetVal(ReturnVal.getInputBitVector())
       # Just remove this extract op
       Block.eraseOperation(ReturnVal)
-    elif not isinstance(ReturnVal.getInputBitVector(), RoseOperation):
+    elif not isinstance(ReturnVal.getInputBitVector(), RoseOperation) \
+      and not isinstance(ReturnVal.getInputBitVector(), RoseArgument):
       # Get all the uses of the input vector of the return value
       Users = Function.getUsersOf(ReturnVal.getInputBitVector())
-      NewReturnValue = RoseValue.create(Context.genName(), ReturnVal.getType())
-      print("NewReturnValue:")
-      NewReturnValue.print()
+      NewReturnValue = RoseValue.create(Context.genName("%" + "dst"), ReturnVal.getType())
       # Replace all the return value input bitvectors with the new one.
       for User in Users:
-        print("USER:")
-        User.print()
-        print("ReturnVal.getInputBitVector():")
-        ReturnVal.getInputBitVector().print()
         Index = User.getIndexForOperand(ReturnVal.getInputBitVector())
         assert isinstance(Index, int)
         User.setOperand(Index, NewReturnValue)
-        print("NEW USER:")
-        User.print()
       # Just remove this extract op
       assert ReturnVal.getNumUsers() == 1
-      Function.print()
-      print("--ReturnVal:")
-      ReturnVal.print()
       ReturnVal.replaceUsesWith(NewReturnValue)
       Function.setRetVal(NewReturnValue)
-      print("---++++FUNCTION:")
-      Function.print()
-      Block.print()
       Block.eraseOperation(ReturnVal)
-      print("BLOCK:")
-      Block.print()
-  
+  else:
+    # Now let's go through all the blocks and look for temporary registers that are
+    # written to and count the number of bvinserts and size of the inserted slices.
+    BlockToInsertsToTempRegs = dict()
+    BlockList = Function.getRegionsOfType(RoseBlock)
+    for Block in BlockList:
+      # We will deal with nested functions later
+      if Block.getFunction() != Function:
+        continue
+      # bvinserts to a temp register must be all in one block. We assert that here.
+      TempRegToBlock = dict()
+      TempReg = RoseUndefValue()
+      for Op in Block:
+        if isinstance(Op, RoseBVInsertSliceOp):
+          if Op.getInputBitVector() != Function.getReturnValue():
+            print("TempReg:")
+            TempReg.print()
+            if isinstance(TempReg, RoseUndefValue):
+              TempReg = Op.getInputBitVector()
+            else:
+              assert TempReg == Op.getInputBitVector()
+            if Op.getInputBitVector() not in TempRegToBlock:
+              TempRegToBlock[Op.getInputBitVector()] = Block
+            else:
+              assert Block == TempRegToBlock[Op.getInputBitVector()]
+            if Block not in BlockToInsertsToTempRegs:
+              BlockToInsertsToTempRegs[Block] = [Op]
+            else:
+              BlockToInsertsToTempRegs[Block].append(Op)
+          continue
+        # We do not expect the inserted bitslices to be extracted
+        # in the same block.
+        #if isinstance(Op, RoseBVExtractSliceOp):
+        #  if not isinstance(TempReg, RoseUndefValue):
+        #    if Op.getInputBitVector() == TempReg:
+        #      del TempRegToBlock[TempReg]
+        #      del BlockToInsertsToTempRegs[Block]
+        #      break
+    # Now fix the size of the temp registers if need be.
+    for Block, InsertOps in BlockToInsertsToTempRegs.items():
+      InsertedSize = 0
+      for InsertOp in InsertOps:
+        assert Block == InsertOp.getParent()
+        print("----InsertOp:")
+        InsertOp.print()
+        InsertedSize += InsertOp.getOutputBitwidth()
+        TempReg = InsertOp.getInputBitVector()
+      print("InsertedSize:")
+      print(InsertedSize)
+      print("TempReg.getType().getBitwidth():")
+      print(TempReg.getType().getBitwidth())
+      if TempReg.getType().getBitwidth() != InsertedSize:
+        assert TempReg.getType().getBitwidth() > InsertedSize
+        NewType = RoseBitVectorType.create(InsertedSize)
+        NewTempReg = RoseValue.create(Context.genName(TempReg.getName()), NewType)
+        # Replace all the temp registers with the new one.
+        Users = Function.getUsersOf(TempReg)
+        for User in Users:
+          Index = User.getIndexForOperand(TempReg)
+          assert isinstance(Index, int)
+          User.setOperand(Index, NewTempReg)
+
   # Look for child function
   for Abstraction in Function:
-    print("Abstraction in function:")
-    Abstraction.print()
     # Run the pass on the nested function
     if isinstance(Abstraction, RoseFunction):
+      print("Abstraction:")
+      Abstraction.print()
       RunBVLengthReductionOnFunction(Abstraction, Context.getContextOfChildFunction(Abstraction))
 
 
@@ -78,7 +120,4 @@ def Run(Function : RoseFunction, Context : RoseContext):
   RunBVLengthReductionOnFunction(Function, Context)
   print("\n\n\n\n\n")
   Function.print()
-
-
-
 
