9b8068ad3203175ad2e3d558a99ef332f5da5356
RafaeNoor <abdurrafae98@live.com>
2022-10-19 16:40:34 -0500

    Adding sobel-X86-256 bit expression for parsing rosette expression

---
+++ code-synthesizer/expr-references/sobel256/hydride.node.10.rkt
@@ -0,0 +1,11 @@
+; hydride.node.10
+; (reg 0) <16 x i32>
+; (reg 1) <1 x i32>
+
+
+(_m_paddd_dsl 
+(_mm512_broadcastd_epi32_dsl 
+(reg 1)  512  512  512  32  0  0  0  );<16 x i32>
+  
+(reg 0)  512  512  512  32  0  0  );<16 x i32>
+
