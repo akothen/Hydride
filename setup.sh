4dfda83a78966ab94e8704c9c4235b2e9fc7fdcd
Akash Kothari <akashk4@illinois.edu>
2022-10-26 09:27:18 -0500

    Fix the script that sets env variables

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 CUR_DIR=$(pwd)
 
 export PYTHONPATH=$CUR_DIR/codegen-generator/targets/x86/:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/targets/hexagon/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/codegen/rosette/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/codegen/llvm/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/rosette-ir/:$PYTHONPATH
@@ -11,8 +12,11 @@ export PYTHONPATH=$CUR_DIR/codegen-generator/tools/fuzzer:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/tools/rosette-lifter/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/tools/low-level-codegen/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/tools/similarity-checker:$PYTHONPATH
+export PYTHONPATH=$CUR_DIR/codegen-generator/tools/validity-checker:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/transform:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/llvmlite:$PYTHONPATH
 
-export PATH=/home/akashk4/Hydride/llvm-project/build/bin/:$PATH
-export PATH=/home/akashk4/Hydride/rosette/bin/:$PATH
+export PATH=$CUR_DIR/llvm-project/build/bin/:$PATH
+export PATH=$CUR_DIR/rosette/bin/:$PATH
+export PATH=$CUR_DIR/bin/:$PATH
+
