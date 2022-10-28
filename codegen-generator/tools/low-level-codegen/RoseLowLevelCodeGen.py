4697ee626f9cabcefb4c1c28a9048fd7825598d5
Akash Kothari <akashk4@illinois.edu>
2022-10-28 14:53:44 -0500

    Make low-level code generator more generalized

--- codegen-generator/tools/low-level-codegen/RoseLowLevelCodeGen.py
+++ codegen-generator/tools/low-level-codegen/RoseLowLevelCodeGen.py
@@ -9,9 +9,16 @@ from RosetteLifter import RosetteLifter
 from RoseLLVMCodeGen import LLVMCodeGen
 
 import sys
+import os
 
 
-def HandleLowLevelCodeGen(RosetteFileName : str, LLVMModuleName : str = None):
+# ~/Hydride/llvm-project/build/lib/LLVMHydride.so
+# x86_wrappers.c.ll
+# -x86-hydride-legalize
+
+def HandleLowLevelCodeGen(RosetteFileName : str, PathToLegalizerLib : str, \
+                          PathToWrapperFile : str, LegalizationFlag : str, \
+                          LLVMModuleName : str = None):
   # Build the Rose IR function and Rose LLVM context from Rosette code
   RosetteFile = open(RosetteFileName, "r")
   RosetteCode = list()
@@ -31,14 +38,42 @@ def HandleLowLevelCodeGen(RosetteFileName : str, LLVMModuleName : str = None):
   Module = open(LLVMIRModule.name + ".ll", "w")
   Module.write(LLVMIRModule.__repr__())
   Module.close()
+  # Legalize code
+  print("EXECUTING:")
+  OriginalLLVMModuleName = LLVMIRModule.name + ".ll"
+  LinkedLLVMBCModuleName = LLVMIRModule.name + ".linked.bc"
+  Command = "llvm-link  {} {} -o {}".format(OriginalLLVMModuleName, PathToWrapperFile, LinkedLLVMBCModuleName)
+  print(Command)
+  os.system(Command)
+  print("EXECUTING:")
+  LinkedLLVMModuleName = LLVMIRModule.name + ".linked.ll"
+  Command = "llvm-dis  {} -o {}".format(LinkedLLVMBCModuleName, LinkedLLVMModuleName)
+  print(Command)
+  os.system(Command)
+  print("EXECUTING:")
+  LegalizeLLVMModuleName = LLVMIRModule.name + ".legalize.ll"
+  Command = "opt -load {} -enable-new-pm=0 {} {} -S -o {}".format(PathToLegalizerLib, LegalizationFlag, \
+                                                          LinkedLLVMModuleName, LegalizeLLVMModuleName)
+  print(Command)
+  os.system(Command)
 
 
+
+# ARG 1: Rosette code to be lifted
+# ARG 2: Path to Shared library that is the target-specific legalizer
+# ARG 3: Path to LLVM wrapper file for a target (x86_wrappers.c.ll)
+# ARG 4: Legalization flag (example: -x86-hydride-legalize)
+# ARG 4 (optional): Name of LLVM module to be generated
 if __name__ == '__main__':
   assert len(sys.argv[1:]) > 0
   RosetteFileName = sys.argv[1]
-  if len(sys.argv[1:]) == 1:
-    HandleLowLevelCodeGen(RosetteFileName)
+  PathToLegalizerLib = sys.argv[2]
+  PathToWrapperFile = sys.argv[3]
+  LegalizationFlag = sys.argv[4]
+  if len(sys.argv[1:]) == 4:
+    HandleLowLevelCodeGen(RosetteFileName, PathToLegalizerLib, LegalizationFlag, PathToWrapperFile)
   else:
-    LLVMModuleName = sys.argv[2]
-    HandleLowLevelCodeGen(RosetteFileName, LLVMModuleName)
+    LLVMModuleName = sys.argv[3]
+    HandleLowLevelCodeGen(RosetteFileName, PathToLegalizerLib, PathToWrapperFile, \
+                        LegalizationFlag, LLVMModuleName)
 
