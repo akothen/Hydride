b9207f95373cf685e6783ede5bdb36849e789a9a
RafaeNoor <abdurrafae98@live.com>
2022-11-06 11:10:19 -0600

    Pushing schedule for matmul_256_32bit

--- hexagon-benchmarks/rake/benchmarks/hexagon/halide/matmul_256_32bit/src/matmul_256_32bit_generator.cpp
+++ hexagon-benchmarks/rake/benchmarks/hexagon/halide/matmul_256_32bit/src/matmul_256_32bit_generator.cpp
@@ -24,11 +24,42 @@ class MatrixMultiply256 : public Generator<MatrixMultiply256> {
 
             RDom k(0, matrix_size);
             Var x("x"), y("y");
+            matrix_mul(x, y) += cast<int32_t>(A(k, y)) * cast<int32_t>(B(x,k));
 
-            matrix_mul(x, y) += A(k, y) * B(x,k);
+
+            res(x, y) = matrix_mul(x, y);
 
 
-            res(x, y) = cast<int32_t>(matrix_mul(x, y));
+
+            Var xi("xi");
+            Var xii("xii");
+            Var yi("yi");
+            RVar r8_x(matrix_mul.update(0).get_schedule().dims()[0].var);
+            res
+                .split(x, x, xi, 256, TailStrategy::ShiftInwards)
+                .split(y, y, yi, 4, TailStrategy::ShiftInwards)
+                .split(xi, xi, xii, 16, TailStrategy::ShiftInwards)
+                .unroll(yi)
+                .vectorize(xii)
+                .compute_root()
+                .reorder({xii, yi, xi, x, y})
+                //.fuse(x, y, x)
+                //.parallel(x);
+                ;
+            matrix_mul.update(0)
+                .split(x, x, xi, 16, TailStrategy::GuardWithIf)
+                .unroll(y)
+                .vectorize(xi)
+                .reorder({xi, x, y, r8_x})
+                .unroll(r8_x,2)
+                ;
+            matrix_mul
+                .store_in(MemoryType::Stack)
+                .split(x, x, xi, 16, TailStrategy::RoundUp)
+                .unroll(y)
+                .vectorize(xi)
+                .compute_at(res, xi)
+                .reorder({xi, x, y});
 
 
             res.bound(x,0, matrix_size)
