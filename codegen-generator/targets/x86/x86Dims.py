0bb89abc55fcd5e7210484acffcc4e6624d48ad7
Kunal Sheth <kunal@kunalsheth.info>
2022-11-30 19:20:16 -0600

    Comment x86Dims.py

--- codegen-generator/targets/x86/x86Dims.py
+++ codegen-generator/targets/x86/x86Dims.py
@@ -11,8 +11,13 @@ from RoseTypes import RoseIntegerType
 
 # Define dimension names used in x86 pseudocodes
 x86Dims = {
-    'rows': RoseConstant.create(16, RoseIntegerType.create(32)),
-    'colsb': RoseConstant.create(512 / 8, RoseIntegerType.create(32)),
+    # AMX section 3.4 of Arch. Instr. Set Ext. Programming Ref.
+    # todo: KUNAL - make these all function params.
+    'rows': RoseConstant.create(10, RoseIntegerType.create(8)),  # for testing...
+    'colsb': RoseConstant.create(10, RoseIntegerType.create(16)),  # for testing...
+
+    # 'max_rows': RoseConstant.create(16, RoseIntegerType.create(8)),
+    # 'max_colsb': RoseConstant.create(512 // 8, RoseIntegerType.create(16)),
 }
 
 
