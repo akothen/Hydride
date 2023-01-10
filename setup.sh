ae3d693360facedd86f8ebfcba7c712cc7478987
Akash Kothari <akashk4@illinois.edu>
2023-01-10 08:45:00 -0600

    Add more environment variables in install script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 
 CUR_DIR=$(pwd)
 
+export HYDRIDE_ROOT=$CUR_DIR
+export LLVM_ROOT=$CUR_DIR/backends/llvm/build/
+export LLVM_DIS_ROOT=$LLVM_ROOT
+export LLVM_CONFIG=$LLVM_DIS_ROOT/bin/llvm-config
+export HALIDE_DISTRIB=$CUR_DIR/frontends/halide/build
+
+export INTRINSICS_LL=$CUR_DIR/code-synthesizer/dsl-ir/x86_wrappers.c.ll
+export LEGALIZER_PATH=$CUR_DIR/backends/llvm/build/lib/LLVMX86Legalizer.so
+
 export PYTHONPATH=$CUR_DIR/codegen-generator/targets/x86/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/targets/hexagon/:$PYTHONPATH
 export PYTHONPATH=$CUR_DIR/codegen-generator/codegen/rosette/:$PYTHONPATH
@@ -20,4 +29,3 @@ export PYTHONPATH=$CUR_DIR/code-synthesizer/dsl-ir:$PYTHONPATH
 export PATH=$CUR_DIR/llvm-project/build/bin/:$PATH
 export PATH=$CUR_DIR/rosette/bin/:$PATH
 export PATH=$CUR_DIR/bin/:$PATH
-
