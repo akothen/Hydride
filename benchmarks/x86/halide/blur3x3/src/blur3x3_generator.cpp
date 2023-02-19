db0ec09e33fcf025d34261b3ff4df769a72375f6
Akash Kothari <akashk4@illinois.edu>
2023-02-19 02:29:36 -0600

    Add hand-tuned blur3x3 implementation

---
+++ benchmarks/x86/halide/blur3x3/src/blur3x3_generator.cpp
@@ -0,0 +1,47 @@
+#include "Halide.h"
+
+
+using namespace Halide;
+
+class HalideBlur : public Halide::Generator<HalideBlur> {
+public:
+    Input<Buffer<uint16_t>>  input{"input", 2};
+    Output<Buffer<uint16_t>> blur_y{"blur_y", 2};
+
+    void generate() {
+        read_input(x, y) = BoundaryConditions::repeat_edge(input)(x, y);
+        blur_x(x, y) = (read_input(x, y) + read_input(x + 1, y) + read_input(x + 2, y)) / 3;
+        blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
+
+        // Schedules for x86
+        blur_y
+            .compute_root()
+            .split(y, y, yi, 54, TailStrategy::ShiftInwards)
+            .split(yi, yi, yii, 9, TailStrategy::ShiftInwards)
+            .split(x, x, xi, 128, TailStrategy::ShiftInwards)
+            .split(xi, xi, xii, 32, TailStrategy::RoundUp)
+            .vectorize(xii, 32);
+            //.parallel(y);
+        blur_x
+            .store_in(MemoryType::Stack)
+            .compute_at(blur_y, yi)
+            .split(x, x, xi, 32, TailStrategy::RoundUp)
+            .vectorize(xi, 32);
+        read_input
+            .store_in(MemoryType::Stack)
+            .store_at(blur_y, y)
+            .compute_at(blur_y, yi)
+            .split(x, x, xi, 32, TailStrategy::RoundUp)
+            .vectorize(xi, 32);
+
+        blur_y.print_loop_nest();
+    }
+
+    void schedule() {}
+
+private:
+    Func blur_x{"blur_x"}, read_input{"read_input"};
+    Var x{"x"}, y{"y"}, xi{"xi"}, yi{"yi"}, yii{"yii"}, xii{"xii"};
+};
+
+HALIDE_REGISTER_GENERATOR(HalideBlur, blur3x3)
