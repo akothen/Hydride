15f59ca92297a9505c139b66bd4980edc8ef0041
Akash Kothari <akashk4@illinois.edu>
2023-02-28 22:56:01 -0600

    Fix schedules for gaussian3x3

--- benchmarks/x86/halide/gaussian3x3/src/gaussian3x3_generator.cpp
+++ benchmarks/x86/halide/gaussian3x3/src/gaussian3x3_generator.cpp
@@ -8,12 +8,8 @@ public:
     Output<Buffer<uint8_t>> output{"output", 2};
 
     void generate() {
-        Func read_input("read_input");
-        read_input = Halide::BoundaryConditions::repeat_edge(input,
-                {{input.dim(0).min(), input.dim(0).extent()},
-                {input.dim(1).min(), input.dim(1).extent()}}
-                );
-        input_16(x, y) = cast<int16_t>(read_input(x, y));
+        bounded_input(x, y) = BoundaryConditions::repeat_edge(input)(x, y);
+        input_16(x, y) = cast<int16_t>(bounded_input(x, y));
 
         rows(x, y) = input_16(x, y-1) + 2 * input_16(x, y) + input_16(x, y+1);
         cols(x,y) =  rows(x-1, y) + 2 * rows(x, y) + rows(x+1, y);
@@ -22,9 +18,19 @@ public:
 
         // Schedules for x86
         output
-            .tile(x, y, xi, yi, 64, 4, TailStrategy::RoundUp)
-            .vectorize(xi, 64)
+            .tile(x, y, xi, yi, 32, 4, TailStrategy::RoundUp)
+            .vectorize(xi, 32)
             .unroll(yi);
+        rows
+            .compute_at(output, y)
+            .tile(x, y, x, y, xi, yi, 32, 4, TailStrategy::RoundUp)
+            .vectorize(xi, 32)
+            .unroll(yi)
+            .align_storage(x, 32);
+        bounded_input
+            .compute_at(output, y)
+            .align_storage(x, 32)
+            .vectorize(x, 32, TailStrategy::RoundUp);
 
         output.print_loop_nest();
     }
@@ -33,7 +39,7 @@ public:
 
 private:
     Var x{"x"}, y{"y"}, xi{"xi"}, xii{"xii"}, yi{"yi"}, yii{"yii"};
-    Func rows{"rows"}, cols{"cols"}, input_16{"input_16"};
+    Func rows{"rows"}, cols{"cols"}, input_16{"input_16"}, bounded_input{"bounded_input"};
 };
 
 HALIDE_REGISTER_GENERATOR(Gaussian3x3, gaussian3x3);
