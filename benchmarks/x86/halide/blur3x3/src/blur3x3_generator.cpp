f507ae94248c018f45a0a7433e91a13fec890b3c
Akash Kothari <akashk4@illinois.edu>
2023-03-03 10:55:00 -0600

    Simplify schedules for blur3x3 (but get same perf)

--- benchmarks/x86/halide/blur3x3/src/blur3x3_generator.cpp
+++ benchmarks/x86/halide/blur3x3/src/blur3x3_generator.cpp
@@ -11,28 +11,23 @@ public:
     void generate() {
         read_input(x, y) = BoundaryConditions::repeat_edge(input)(x, y);
         blur_x(x, y) = (read_input(x, y) + read_input(x + 1, y) + read_input(x + 2, y)) / 3;
+        //blur_x(x, y) = (input(x, y) + input(x+1, y) + input(x+2, y))/3;
         blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
 
         // Schedules for x86
         blur_y
-            .compute_root()
-            .split(y, y, yi, 54, TailStrategy::ShiftInwards)
-            .split(yi, yi, yii, 9, TailStrategy::ShiftInwards)
-            .split(x, x, xi, 128, TailStrategy::ShiftInwards)
-            .split(xi, xi, xii, 32, TailStrategy::RoundUp)
-            .vectorize(xii, 32);
-            //.parallel(y);
+            .split(y, y, yi, 32)
+            .vectorize(x, 32);
         blur_x
             .store_in(MemoryType::Stack)
-            .compute_at(blur_y, yi)
-            .split(x, x, xi, 32, TailStrategy::RoundUp)
-            .vectorize(xi, 32);
+            .store_at(blur_y, y)
+            .compute_at(blur_y, x)
+            .vectorize(x, 32);
         read_input
             .store_in(MemoryType::Stack)
             .store_at(blur_y, y)
-            .compute_at(blur_y, yi)
-            .split(x, x, xi, 32, TailStrategy::RoundUp)
-            .vectorize(xi, 32);
+            .compute_at(blur_y, y)
+            .vectorize(x, 32);
 
         blur_y.print_loop_nest();
     }
