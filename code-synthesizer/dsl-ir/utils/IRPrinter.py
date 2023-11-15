f700d4a45f43f1e00b8ba8af1173ca76b2da6df4
RafaeNoor <abdurrafae98@live.com>
2023-11-15 16:59:56 -0600

    Pushing more local changes

--- code-synthesizer/dsl-ir/utils/IRPrinter.py
+++ code-synthesizer/dsl-ir/utils/IRPrinter.py
@@ -3,67 +3,63 @@ from common.Types import *
 from common.PredefinedDSL import *
 import sys
 
-
 class IRPrinter:
 
-    def __init__(self, printer_name="hydride-printer"):
+    def __init__(self, printer_name = "hydride-printer", get_length_name = "get-length", get_prec_name = "get-prec"):
 
         self.printer_name = printer_name
+        self.get_length_name = get_length_name
+        self.get_prec_name = get_prec_name
 
-    def emit_default_def(self, struct_definer):
+    def emit_default_def(self, struct_definer ):
         defaults = []
 
-        defaults.append(
-            "[(dim-x id) (string-append \"\\n\"  \"(dim-x \" (~s id) \")\")]")
-        defaults.append(
-            "[(dim-y id) (string-append \"\\n\" \"(dim-y \" (~s id) \")\")]")
-        defaults.append(
-            "[(idx-i id) (string-append \"\\n\" \"(idx-i \" (~s id) \")\")]")
-        defaults.append(
-            "[(idx-j id) (string-append \"\\n\" \"(idx-j \" (~s id) \")\")]")
-        defaults.append(
-            "[(reg id) (string-append  \"\\n\" \"(reg \" (~s  (bitvector->natural id)) \")\")]")
+        defaults.append("[(dim-x id) (string-append \"\\n\"  \"(dim-x \" (~s id) \")\")]")
+        defaults.append("[(dim-y id) (string-append \"\\n\" \"(dim-y \" (~s id) \")\")]")
+        defaults.append("[(idx-i id) (string-append \"\\n\" \"(idx-i \" (~s id) \")\")]")
+        defaults.append("[(idx-j id) (string-append \"\\n\" \"(idx-j \" (~s id) \")\")]")
+        defaults.append("[(reg id) (string-append  \"\\n\" \"(reg \" (~s  (bitvector->natural id)) \")\")]")
 
         defaults.append("[(lit v) (string-append   \"(lit \" (~s v) \")\")]")
 
-        defaults.append(
-            "[(nop v1) (string-append \"\\n\" \"(nop \" ({} v1) \")\")]".format(self.printer_name))
-        defaults.append(
-            "[(idx-add i1 i2) (string-append \"\\n\" \"(idx-add \" (~s i1) (~s i2) \")\" )]")
-        defaults.append(
-            "[(idx-mul i1 i2) (string-append \"\\n\" \"(idx-mul \" (~s i1) (~s i2) \")\" )]")
+        defaults.append("[(nop v1) (string-append \"\\n\" \"(nop \" ({} v1) \")\")]".format( self.printer_name))
+        defaults.append("[(idx-add i1 i2) (string-append \"\\n\" \"(idx-add \" (~s i1) (~s i2) \")\" )]")
+        defaults.append("[(idx-mul i1 i2) (string-append \"\\n\" \"(idx-mul \" (~s i1) (~s i2) \")\" )]")
 
-        # defaults.append(self.emit_get_len_def(dummy_vector_load_dsl, struct_definer)[1:])
+        #defaults.append(self.emit_get_len_def(dummy_vector_load_dsl, struct_definer)[1:])
+
 
         # Special case handling for vector loads
 
-        load_dsl_use = struct_definer.emit_dsl_struct_use(
-            dummy_vector_load_dsl)
+        load_dsl_use = struct_definer.emit_dsl_struct_use(dummy_vector_load_dsl)
         load_args = dummy_vector_load_dsl.get_sample_context().context_args
-        load_len_str = "(string-append \"({} \"".format(
-            dummy_vector_load_dsl.get_dsl_name())
-        load_len_expr = "(* {} {})".format(
-            load_args[3].name, load_args[4].name)
+        load_len_str = "(string-append \"({} \"".format(dummy_vector_load_dsl.get_dsl_name())
+        load_len_expr = "(* {} {})".format(load_args[3].name, load_args[4].name)
 
-        # defaults.append(self.emit_print_clause(dummy_vector_load_dsl, struct_definer))
+        #defaults.append(self.emit_print_clause(dummy_vector_load_dsl, struct_definer))
 
         # Special case handling for two input swizzle
 
-        # defaults.append(self.emit_print_clause(dummy_vector_swizzle_dsl, struct_definer))
+        #defaults.append(self.emit_print_clause(dummy_vector_swizzle_dsl, struct_definer))
+
 
         for structs in default_structs:
             if structs.name == "llvm_shuffle_vectors":
                 continue
 
+
             defaults.append(self.emit_print_clause(structs, struct_definer))
+
+
 
         return ["\t{}".format(d) for d in defaults]
 
     def emit_print_clause(self, dsl_inst, struct_definer):
         return "[{} \n\t(string-append \"\\n\" \n\t{}\n\t{} \"\\n\")]".format(struct_definer.emit_dsl_struct_use(dsl_inst),
-                                                                              self.emit_base_print_expr(
-                                                                                  dsl_inst),
-                                                                              self.emit_output_type_str(dsl_inst, struct_definer))
+                                                   self.emit_base_print_expr(dsl_inst),
+                                                   self.emit_output_type_str(dsl_inst, struct_definer, get_len_name = self.get_length_name, get_prec_name = self.get_prec_name))
+
+
 
     def emit_bounded_base_print_expr(self, dsl_inst):
         sample_ctx = dsl_inst.get_sample_context()
@@ -77,45 +73,51 @@ class IRPrinter:
 
         define_prec_str = "(define prec-arg (cond\n"
 
+
         for ip in sample_ctx.in_bound_map:
-            define_prec_str += "[(equal? {} (bitvector->integer {}))  {}]\n".format(
-                ip, ir_args[variable_idx].name, ip)
+            define_prec_str += "[(equal? {} (bitvector->integer {}))  {}]\n".format(ip, ir_args[variable_idx].name, ip)
+
 
         define_prec_str += "))\n"
 
-        input_prec_arg = Integer("prec-arg", value=16)
+
+        input_prec_arg = Integer("prec-arg", value = 16)
+
+
+
 
         str_expr = define_prec_str
         str_expr += "(string-append \"({} \" ".format(dsl_inst.get_dsl_name())
 
+
+
+
+
+
+
         for ir_arg in ir_args:
             # Literals don't have any precision information generated during synthesis, hence we use
             # the user of the literals value to identify its type information.
-            if isinstance(ir_arg,  ConstBitVector):
-                hack_size = 64 if ir_arg.size == 192 else ir_arg.size
-                expr_typeinfo = "(string-append ({} {})  \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(
-                    self.printer_name, ir_arg.name, "(~s 1)", f"(~s {hack_size})")
-                
 
-                lit_typeinfo = "(begin (define-values (num_elem arg_prec) (values 1 {}) )".format(hack_size)
-                lit_typeinfo += "(string-append ({} {}) \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(
-                    self.printer_name, ir_arg.name, "(~s num_elem)", "(~s " + "arg_prec" + ")")
-                lit_typeinfo += ")"
+            lit_typeinfo = ""
+
+            # Fallback to default printer
+            # as DSL inst provides no precision information
+            if input_prec_arg == None :
+                lit_typeinfo = "({} {})".format(self.printer_name, ir_arg.name)
             else:
-                lit_typeinfo = "(begin (define-values (num_elem arg_prec) (cond [(equal? (get-length {} (vector 0)) 32)   (values 1 32)] [(equal? (get-length {} (vector 0)) 192)   (values 1 192)] [(< (/ (get-length {} (vector 0)) {}) 1)  (values  (/ (get-length {} (vector 0)) (get-prec {} (vector 0))) (/ (get-prec {} (vector 0))) )] [else (values (/ (get-length {} (vector 0)) {}) {})]))".format(
-                    ir_arg.name, ir_arg.name, ir_arg.name, input_prec_arg.name, ir_arg.name, ir_arg.name, ir_arg.name, ir_arg.name, input_prec_arg.name, input_prec_arg.name)
-                # lit_typeinfo = "(begin (define num_elem (/ (get-length {} (vector 0)) {})) ".format(ir_arg.name, input_prec_arg.name)
-                lit_typeinfo += "(string-append ({} {}) \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(
-                    self.printer_name, ir_arg.name, "(~s num_elem)", "(~s " + "arg_prec" + ")")
+                lit_typeinfo = "(begin (define-values (num_elem arg_prec) (cond [(< (/ (get-length {} (vector 0)) {}) 1)  (values  (/ (get-length {} (vector 0)) (get-prec {} (vector 0))) (/ (get-prec {} (vector 0))) )] [else (values (/ (get-length {} (vector 0)) {}) {})]))".format(ir_arg.name, input_prec_arg.name, ir_arg.name, ir_arg.name, ir_arg.name, ir_arg.name, input_prec_arg.name, input_prec_arg.name)
+                #lit_typeinfo = "(begin (define num_elem (/ (get-length {} (vector 0)) {})) ".format(ir_arg.name, input_prec_arg.name)
+                lit_typeinfo += "(string-append ({} {}) \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(self.printer_name, ir_arg.name, "(~s num_elem)", "(~s " + "arg_prec" +")")
                 lit_typeinfo += ")"
 
-            conditional_printer = "(if (lit? {}) {} ({} {})) \" \"".format(
-                ir_arg.name, lit_typeinfo, self.printer_name, ir_arg.name)
+
+            conditional_printer = "(if (lit? {}) {} ({} {})) \" \"".format(ir_arg.name, lit_typeinfo, self.printer_name, ir_arg.name)
             str_expr += "{} \" \" ".format(conditional_printer)
 
         str_expr += "\")\")"
 
-        return "(begin " + str_expr + ")\n"
+        return "(begin "+ str_expr +")\n"
 
     def emit_base_print_expr(self, dsl_inst):
 
@@ -127,6 +129,7 @@ class IRPrinter:
 
         input_prec_arg = None
 
+
         # Identify argument corresponding to input precision
         for ir_arg in ir_args:
             if isinstance(ir_arg, Precision) and ir_arg.input_precision:
@@ -137,18 +140,21 @@ class IRPrinter:
                 input_prec_arg = ir_arg
                 break
 
+
         # Similarity checking may have folded
         # the argument when all are the same
         if input_prec_arg == None:
             input_precisions = [ctx.in_precision for ctx in dsl_inst.contexts]
             ip_0 = input_precisions[0]
-            all_same = all([(ip == ip_0) and (ip != None)
-                           for ip in input_precisions])
+            all_same = all([(ip == ip_0) and (ip != None) for ip in input_precisions])
 
             if all_same:
-                input_prec_arg = Integer(str(ip_0), value=ip_0)
+                input_prec_arg = Integer(str(ip_0), value = ip_0)
 
-        # assert input_prec_arg != None, "DSL Inst must contain input precision argument "+dsl_inst.name
+
+
+
+        #assert input_prec_arg != None, "DSL Inst must contain input precision argument "+dsl_inst.name
 
         for ir_arg in ir_args:
             # Literals don't have any precision information generated during synthesis, hence we use
@@ -158,62 +164,66 @@ class IRPrinter:
 
             # Fallback to default printer
             # as DSL inst provides no precision information
-            if input_prec_arg == None:
+            if input_prec_arg == None :
                 lit_typeinfo = "({} {})".format(self.printer_name, ir_arg.name)
             else:
-                if isinstance(ir_arg,  ConstBitVector):
-                    hack_size = 64 if ir_arg.size == 192 else ir_arg.size
-                    expr_typeinfo = "(string-append ({} {})  \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(
-                        self.printer_name, ir_arg.name, "(~s 1)", f"(~s {hack_size})")
-                    
+                lit_typeinfo = "(begin (define-values (num_elem arg_prec) (cond [(< (/ ({} {} (vector 0)) {}) 1)  (values  (/ ({} {} (vector 0)) ({} {} (vector 0))) (/ ({} {} (vector 0))) )] [else (values (/ ({} {} (vector 0)) {}) {})]))".format(self.get_length_name, ir_arg.name, input_prec_arg.name, self.get_length_name ,ir_arg.name, self.get_prec_name, ir_arg.name, self.get_prec_name , ir_arg.name, self.get_length_name, ir_arg.name, input_prec_arg.name, input_prec_arg.name)
+                lit_typeinfo += "(string-append ({} {}) \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(self.printer_name, ir_arg.name, "(~s num_elem)", "(~s " + "arg_prec" +")")
+                lit_typeinfo += ")"
 
-                    lit_typeinfo = "(begin (define-values (num_elem arg_prec) (values 1 {}) )".format(hack_size)
-                    lit_typeinfo += "(string-append ({} {}) \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(
-                        self.printer_name, ir_arg.name, "(~s num_elem)", "(~s " + "arg_prec" + ")")
-                    lit_typeinfo += ")"
-                else:
-                    lit_typeinfo = "(begin (define-values (num_elem arg_prec) (cond [(equal? (get-length {} (vector 0)) 32)   (values 1 32)] [(equal? (get-length {} (vector 0)) 192)   (values 1 192)] [(< (/ (get-length {} (vector 0)) {}) 1)  (values  (/ (get-length {} (vector 0)) (get-prec {} (vector 0))) (/ (get-prec {} (vector 0))) )] [else (values (/ (get-length {} (vector 0)) {}) {})]))".format(
-                        ir_arg.name, ir_arg.name, ir_arg.name, input_prec_arg.name, ir_arg.name, ir_arg.name, ir_arg.name, ir_arg.name, input_prec_arg.name, input_prec_arg.name)
-                    # lit_typeinfo = "(begin (define num_elem (/ (get-length {} (vector 0)) {})) ".format(ir_arg.name, input_prec_arg.name)
-                    lit_typeinfo += "(string-append ({} {}) \" ; \" \"<\" {} \" x i\" {} \">\" \"\\n\" )".format(
-                        self.printer_name, ir_arg.name, "(~s num_elem)", "(~s " + "arg_prec" + ")")
-                    lit_typeinfo += ")"
 
-            conditional_printer = "(if (lit? {}) {} ({} {})) \" \"".format(
-                ir_arg.name, lit_typeinfo, self.printer_name, ir_arg.name)
+            conditional_printer = "(if (lit? {}) {} ({} {})) \" \"".format(ir_arg.name, lit_typeinfo, self.printer_name, ir_arg.name)
             str_expr += "{} \" \" ".format(conditional_printer)
 
         str_expr += "\")\")"
 
         return str_expr
 
-    def emit_output_type_str(self, dsl_inst, struct_definer, get_len_name="get-length", get_prec_name="get-prec", prog_name="prog"):
+
+    def emit_output_type_str(self, dsl_inst, struct_definer, get_len_name = "get-length", get_prec_name = "get-prec" , prog_name = "prog"):
         out_prec_arg = None
 
-        get_prec_expr = "({} {} (vector 0))".format(get_prec_name, prog_name)
-        num_elem_str = "(~s (/ ({} {} (vector 0)) {}) )".format(get_len_name,
-                                                                prog_name, get_prec_expr)
+
+
+        get_prec_expr = "({} {} (vector 0))".format(get_prec_name, prog_name )
+        num_elem_str = "(~s (/ ({} {} (vector 0)) {}) )".format(get_len_name, prog_name, get_prec_expr)
         prec_str = "(~s {})".format(get_prec_expr)
 
+
+
+
         return "(string-append \";\" \"<\" {} \" x \" \"i\" {} \">\")".format(num_elem_str, prec_str)
+
 
     def emit_fallback_def(self):
         return "\t[v (~s v)]"
 
-    def emit_dsl_printer(self, dsl_inst_list, struct_definer, prog_name="prog"):
+
+
+
+
+
+
+
+
+    def emit_dsl_printer(self, dsl_inst_list, struct_definer, prog_name = "prog"):
         print_clauses = self.emit_default_def(struct_definer)
 
-        print_clauses += [self.emit_print_clause(dsl_inst, struct_definer)
-                          for dsl_inst in dsl_inst_list]
+        print_clauses += [self.emit_print_clause(dsl_inst, struct_definer) for dsl_inst in dsl_inst_list]
 
         print_clauses += [self.emit_fallback_def()]
 
+
+
         prefix = ";; "+"="*80 + "\n"
-        prefix += ";; "+" "*30 + " DSL Custom Printer"+'\n'
+        prefix += ";; "+" "*30 +" DSL Custom Printer"+'\n'
         prefix += ";; "+"="*80 + "\n"
 
         sufix = "\n;; "+"="*80 + "\n"
 
-        print_prog = "(define ({}  {})\n (destruct {}\n{}\n )\n)".format(
-            self.printer_name, prog_name, prog_name, "\n".join(print_clauses))
+
+
+        print_prog  = "(define ({}  {})\n (destruct {}\n{}\n )\n)".format(
+            self.printer_name , prog_name, prog_name, "\n".join(print_clauses))
         return prefix + print_prog + sufix
+
