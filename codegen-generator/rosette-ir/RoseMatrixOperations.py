59b2bddd1abe36def1b3680e8a745f4053a40193
Akash K. <akashk4@illinois.edu>
2024-12-04 00:00:24 -0600

    Add support for inserting to multiple tile rows

--- codegen-generator/rosette-ir/RoseMatrixOperations.py
+++ codegen-generator/rosette-ir/RoseMatrixOperations.py
@@ -29,19 +29,25 @@ class RoseMatrixExtractRowOp(RoseMatrixOp):
 
     def getRowIndex(self):
         return self.getOperand(1)
+    
+    def getRowIndexPos(self):
+        return 1
+    
+    def getBitwidthPos(self):
+        return 2
 
     def to_rosette(self, NumSpace=0, ReverseIndexing=False):
         NotImplemented
 
 
 class RoseMatrixInsertRowOp(RoseMatrixOp):
-    def __init__(self, Elements: RoseValue, Tile: RoseValue, Idx: RoseValue, ParentBlock):
-        inputs = [Elements, Tile, Idx]
+    def __init__(self, InsertVal: RoseValue, Tile: RoseValue, Idx: RoseValue, ParentBlock):
+        inputs = [InsertVal, Tile, Idx]
         super().__init__(RoseOpcode.mtxinsertrow, "", inputs, ParentBlock)
 
     @staticmethod
-    def create(Elements: RoseValue, Tile: RoseValue, Idx: RoseValue, ParentBlock=RoseUndefRegion()):
-        return RoseMatrixInsertRowOp(Elements, Tile, Idx, ParentBlock)
+    def create(InsertVal: RoseValue, Tile: RoseValue, Idx: RoseValue, ParentBlock=RoseUndefRegion()):
+        return RoseMatrixInsertRowOp(InsertVal, Tile, Idx, ParentBlock)
 
     def getElements(self):
         return self.getOperand(0)
@@ -59,14 +65,16 @@ class RoseMatrixInsertRowOp(RoseMatrixOp):
         NotImplemented
 
 
-class RoseMatrixInsertElementOp(RoseMatrixOp):
-    def __init__(self, element: RoseValue, Tile: RoseValue, r1: RoseValue, c1: RoseValue, r2: RoseValue, c2: RoseValue, ParentBlock):
-        inputs = [element, Tile, r1, c1, r2, c2]
-        super().__init__(RoseOpcode.mtxinsertel, "", inputs, ParentBlock)
+class RoseMatrixInsertRowsOp(RoseMatrixOp):
+    def __init__(self, InsertVal: RoseValue, Tile: RoseValue, r1: RoseValue, \
+                    r2: RoseValue, ParentBlock):
+        inputs = [InsertVal, Tile, r1, r2]
+        super().__init__(RoseOpcode.mtxinsertrows, "", inputs, ParentBlock)
 
     @staticmethod
-    def create(element: RoseValue, Tile: RoseValue, r1: RoseValue, c1: RoseValue, r2: RoseValue, c2: RoseValue, ParentBlock=RoseUndefRegion()):
-        return RoseMatrixInsertElementOp(element, Tile, r1, c1, r2, c2, ParentBlock)
+    def create(InsertVal: RoseValue, Tile: RoseValue, r1: RoseValue, r2: RoseValue, \
+               ParentBlock=RoseUndefRegion()):
+        return RoseMatrixInsertRowsOp(InsertVal, Tile, r1, r2, ParentBlock)
 
     def getElement(self):
         return self.getOperand(0)
@@ -77,11 +85,9 @@ class RoseMatrixInsertElementOp(RoseMatrixOp):
     def getRowRange(self):
         return self.getOperand(2), self.getOperand(4)
 
-    def getColRange(self):
-        return self.getOperand(3), self.getOperand(5)
-
     def isIndexingBVOp(self):
         return True
 
     def to_rosette(self, NumSpace=0, ReverseIndexing=False):
         NotImplemented
+
